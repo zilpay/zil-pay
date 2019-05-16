2b963a14c5e6ccd7aea668b275962101f4b2d721
Rinat <lich666black@yahoo.com>
2019-05-16 22:51:26 +0500

    style full screan UI

--- src/components/HomeBar.vue
+++ src/components/HomeBar.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="nav-container">
     <nav>
-      <div class="left">
-        <p>ZilPay</p>
+      <div class="left text-white">
+        ZilPay
       </div>
 
       <div class="center">
-
+        
       </div>
 
       <div class="right">
-
+        <img src="/icons/locked.svg" height="30">
+        <img src="/icons/refresh.svg" height="30">
+        <img src="/icons/settings.svg" height="30">
       </div>
 
     </nav>
@@ -24,4 +26,13 @@ export default {
 </script>
 
 <style lang="scss">
+.left {
+  padding: 5%;
+  font-size: 25px;
+  font-weight: bold;
+}
+.right > img {
+  margin-top: 24px;
+  padding: 1%;
+}
 </style>
