8e164d3b22042c15c2dbdd16325de4360bd93923
Rinat <lich666black@yahoo.com>
2019-05-21 13:32:00 +0500

    added close btn for backBar

--- src/components/BackBar.vue
+++ src/components/BackBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav-container">
-    <nav>
+    <nav class="back-bar">
       <img src="/icons/back.svg" height="40"
            class="back"
            @click="$router.go(-1)">
@@ -8,23 +8,43 @@
       <span class="route-name text-black">
         {{$router.currentRoute.name}}
       </span>
+
+      <img class="close" height="30"
+           src="/icons/close-button.svg"
+           v-show="isCloseIcon"
+           @click="$router.push({name: 'Home'})">
     </nav>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BackBar'
+  name: 'BackBar',
+  computed: {
+    isCloseIcon() {
+      const { path } = this.$router.currentRoute;
+      const count = path.split('/').length;
+
+      if (count > 2) {
+        return true;
+      }
+
+      return false;
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-.back {
+.back, .close {
   margin: 10px;
   cursor: pointer;
 }
 .route-name {
   margin: 10px;
   font-weight: 500;
+}
+.back-bar {
+  justify-items: center;
 }
 </style>
