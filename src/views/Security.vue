0758b0f568519fde1a5db437279e9feb49144b70
Rinat <lich666black@yahoo.com>
2019-05-21 15:14:48 +0500

    created Security page

--- src/views/Security.vue
+++ src/views/Security.vue
@@ -2,7 +2,18 @@
   <div>
     <BackBar/>
 
-    <main></main>
+    <main>
+      <div class="form">
+        <h5>Privacy Data</h5>
+        <div class="dis">Clear all allowed accesses to dApp.</div>
+        <button>Clear data</button>
+      </div>
+
+      <div class="form">
+        <h5>Reveal Seed Words</h5>
+        <button>Reveal Seed Words</button>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -15,3 +26,16 @@ export default {
   components: { BackBar }
 }
 </script>
+
+<style lang="scss">
+
+.form {
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: 1fr;
+}
+.dis {
+  font-size: 15px;
+  margin: 10px;
+}
+</style>
