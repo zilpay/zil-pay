fa7f537478eefeeaa07bc46face0e4ab05aaf6a0
Rinat <lich666black@yahoo.com>
2019-05-21 14:58:08 +0500

    created Advanced page

--- src/views/Advanced.vue
+++ src/views/Advanced.vue
@@ -2,7 +2,20 @@
   <div>
     <BackBar/>
 
-    <main></main>
+    <main>
+      <div class="form-border change-gas">
+        <div>
+          <label for="gasPrice">Gas Price (Li)</label>
+          <input type="number" id="gasPrice" value="1000">
+        </div>
+        <div>
+          <label for="gasLimit">Gas Limit</label>
+          <input type="number" id="gasLimit" value="1">
+        </div>
+      </div>
+
+      <button>default</button>
+    </main>
   </div>
 </template>
 
@@ -15,3 +28,12 @@ export default {
   components: { BackBar }
 }
 </script>
+
+<style lang="scss">
+
+.change-gas {
+  margin: 30px;
+  padding: 30px;
+  margin-top: 30px;
+}
+</style>
