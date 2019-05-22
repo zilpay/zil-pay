a58a84df6e09c4862e4b42e2d02a32484a396af6
Rinat <lich666black@yahoo.com>
2019-05-22 21:56:43 +0500

    fixed Restore page

--- src/views/Restore.vue
+++ src/views/Restore.vue
@@ -3,8 +3,8 @@
     <BackBar/>
 
     <main class="restore">
-      <h4>Restore by mnemonic Seed Phrase</h4>
-      <div>
+      <div class="header">
+        <h5>Restore by mnemonic Seed Phrase</h5>
         <label>Enter your secret mnemonic word phrase here.</label>
         <br>
         <textarea class="seed" cols="30" v-model="seed"></textarea>
@@ -13,11 +13,11 @@
       <div class="input-group">
          <div class="text-left">
           <label>Password (min 6 chars)</label>
-          <input type="number">
+          <input type="password">
         </div>
         <div class="text-left">
           <label>Confirm Password</label>
-          <input type="number">
+          <input type="password">
         </div>
 
         <button>Restore</button>
@@ -51,5 +51,8 @@ export default {
 .restore {
   justify-content: center;
   justify-items: center;
+}
+.header {
+  width: 90%;
 }
 </style>
