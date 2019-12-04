f5a814bc0cdfc6ecd0faca2c602eb06bd7c57b9e
Хасаншин Ринат <rinat.h@jetstyle.ru>
2019-12-04 15:38:56 +0500

    added test for EncryptedStream

--- tests/unit/lib/stream/encrypted-stream.spec.js
+++ tests/unit/lib/stream/encrypted-stream.spec.js
@@ -1,11 +1,36 @@
 import { EncryptedStream } from 'lib/stream'
+import { uuid } from 'uuidv4'
 
 describe('lib:stream:EncryptedStream', () => {
+  let streamA = null
+  let streamB = null
+  let synced = false
 
-  const stream = new EncryptedStream()
+  it('should be able instantiate two unique streams', () => {
+    streamA = new EncryptedStream('streamA', uuid())
+    streamB = new EncryptedStream('streamB', uuid())
+    streamB.onSync(() => synced = true)
+    // expect(true).toBe(true)
+  })
 
-  it('renders props.msg when passed', () => {
-    console.log(stream)
-    expect(true).to.be(true)
+  it('should be able to sync the streams and trigger encryption', () => {
+    streamA.sync('streamB', streamA.key)
+
+    expect(streamA.synced).toBeTruthy()
+    expect(streamB.synced).toBeTruthy()
+    expect(synced).toBeTruthy()
+  })
+
+  it('should be able to send messages and receive them decrypted', () => {
+    const payload = {
+      test: 'test'
+    }
+
+    streamB.listenWith((msg) => {
+      expect(msg.from).toEqual('streamA')
+      expect(msg.payload).toEqual(payload)
+    })
+
+    streamA.send({ payload }, 'streamB')
   })
 })
