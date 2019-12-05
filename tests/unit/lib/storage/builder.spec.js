4ff205880c4f99939ceab1f1147e0134011ae964
Хасаншин Ринат <rinat.h@jetstyle.ru>
2019-12-05 13:00:09 +0500

    fixed BuildObject and test for it

--- tests/unit/lib/storage/builder.spec.js
+++ tests/unit/lib/storage/builder.spec.js
@@ -5,4 +5,10 @@ describe('lib:storage:BuildObject', () => {
   it('should be BuildObject', () => {
     expect(new BuildObject('test', 'value')).toBeTruthy()
   })
+
+  it('has key and value', () => {
+    expect(new BuildObject('test', 'value')).toEqual({
+      test: 'value'
+    })
+  })
 })
