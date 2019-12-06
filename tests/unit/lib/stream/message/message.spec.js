30edc6214755814ecb9d266efeb76e2cafcede99
Хасаншин Ринат <rinat.h@jetstyle.ru>
2019-12-06 17:01:11 +0500

    added Message and test

---
+++ tests/unit/lib/stream/message/message.spec.js
@@ -0,0 +1,40 @@
+import extension from 'extensionizer'
+import sinonChrome from 'sinon-chrome'
+import { uuid } from 'uuidv4'
+
+sinonChrome.runtime.id = uuid()
+
+global.chrome = sinonChrome
+
+extension.runtime = global.chrome.runtime
+
+const { Message } = require('lib/stream/message')
+
+describe('lib:stream:message:Message', () => {
+
+  it('test import class Message', () => {
+    expect(Message).toBeTruthy()
+  })
+
+  it('test init class Message', () => {
+    const msg = {
+      type: uuid(),
+      payload: {
+        random: uuid()
+      }
+    }
+    const message = new Message(msg)
+
+    message.send().then(console.log)
+
+    expect(message).toBeTruthy()
+    expect(message.send).toBeTruthy()
+    expect(message.type).toEqual(msg.type)
+    expect(message.payload).toEqual(msg.payload)
+  })
+
+  it('should have static method signal', () => {
+    expect(Message.signal).toBeTruthy()
+  })
+
+})
