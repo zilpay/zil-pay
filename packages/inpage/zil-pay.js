b5598f9b3cc8fdbd4b5e35e07bb516819c43d589
Rinat <lich666black@yahoo.com>
2019-12-07 07:37:51 +0000

    added header to few files

--- packages/inpage/zil-pay.js
+++ packages/inpage/zil-pay.js
@@ -1,3 +1,11 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
 import * as zilUtils from '@zilliqa-js/util'
 import { Blockchain } from '@zilliqa-js/blockchain'
 import { TransactionFactory } from '@zilliqa-js/account'
--- tests/unit/lib/stream/local-stream.spec.js
+++ tests/unit/lib/stream/local-stream.spec.js
@@ -1,16 +1,35 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
 import sinonChrome from 'sinon-chrome'
+import extension from 'extensionizer'
 import { uuid } from 'uuidv4'
+
+jest.useFakeTimers()
 
 sinonChrome.runtime.id = uuid()
 
 global.chrome = sinonChrome
 
+extension.runtime = global.chrome.runtime
+
 const { LocalStream } = require('lib/stream')
+
+// setInterval(() => console.log('work'), 1000)
 
 describe('lib:stream:LocalStream', () => {
 
   it('test import class LocalStream', () => {
     expect(LocalStream).toBeTruthy()
+  })
+
+  it('test import class LocalStream', () => {
+    LocalStream.watch(console.log)
   })
 
   it('should be able send request', () => {
@@ -21,3 +40,4 @@ describe('lib:stream:LocalStream', () => {
   })
 
 })
+jest.advanceTimersByTime(10000)
