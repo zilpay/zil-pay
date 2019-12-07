939bb2f9a3d76d4b3a00352f92eae64c46fba335
Rinat <lich666black@yahoo.com>
2019-12-07 09:26:07 +0000

    fixed content.stream script

--- packages/content/stream.js
+++ packages/content/stream.js
@@ -6,14 +6,43 @@
  * -----
  * Copyright (c) 2019 ZilPay
  */
-import { NonSecureStream } from './localStream'
-import { SecureStream } from './encryptedStream'
+import { NonSecureStream } from './local-stream'
+import { SecureStream } from './encrypted-stream'
+import {
+  SecureMessage,
+  MTypeSecure
+} from 'lib/stream'
 
 export class Stream {
 
   constructor() {
     this.secureStream = new SecureStream()
-    this.nonSecureStream = new NonSecureStream(this.secureStream.stream)
+    this.nonSecureStream = new NonSecureStream()
+
+    this.nonSecureStream._watchTabMessaging(
+      (...args) => this._dispenseMessage(...args)
+    )
+  }
+
+  _dispenseMessage(sendResponse, message) {
+    if (!message) {
+      return null
+    }
+
+    this._broadcastToSecure(message)
+
+    sendResponse(true)
+  }
+
+  _broadcastToSecure(msg) {
+    let toSecureMsg = msg.type
+
+    const recipient = MTypeSecure.INJECTED
+
+    new SecureMessage({
+      type: toSecureMsg,
+      payload: msg.payload
+    }).send(this.secureStream, recipient)
   }
 
 }
