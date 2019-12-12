a83c68441a3274695711a57ea895d794244cb275
Rinat <lich666black@yahoo.com>
2019-12-12 18:01:18 +0000

    added title component

---
+++ src/components/Title.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="b({ variant })">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { COLOR_VARIANTS } from '@/config'
+
+/**
+ * @example
+ * <Title>My content</Title>
+ */
+export default {
+  name: 'Title',
+  props: {
+    variant: {
+      type: String,
+      default: COLOR_VARIANTS.black
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.Title {
+  font-family: var(--font-family-bold);
+  font-size: 48px;
+  line-height: 58px;
+
+  &_variant-black {
+    color: var(--accent-color-black);
+  }
+}
+</style>
