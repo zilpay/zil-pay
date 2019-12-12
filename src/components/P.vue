8cb21afb207000ce4a6de22d01edcd12723446a6
Rinat <lich666black@yahoo.com>
2019-12-12 18:00:51 +0000

    added P or Text componetn

---
+++ src/components/P.vue
@@ -0,0 +1,49 @@
+<template>
+  <p :class="b({ variant, font })">
+    <slot />
+  </p>
+</template>
+
+<script>
+import {
+  COLOR_VARIANTS,
+  FONT_VARIANTS
+} from '@/config'
+
+/**
+ * @example
+ * import { COLOR_VARIANTS, SIZE_VARIANS } from '@/config'
+ * import P from '@/components/P'
+ * <P :variant="COLOR_VARIANTS.black">My content</P>
+ */
+export default {
+  name: 'P',
+  props: {
+    variant: {
+      type: String,
+      default: COLOR_VARIANTS.black
+    },
+    font: {
+      type: String,
+      default: FONT_VARIANTS.light
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.P {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 22px;
+  line-height: 32px;
+
+  &_variant-black {
+    color: var(--accent-color-black);
+  }
+
+  &_font-PoppinsLight {
+    font-family: var(--font-family-light);
+  }
+}
+</style>
