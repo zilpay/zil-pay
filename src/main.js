f55edb8b42a8a14af659f750b8e915643affe647
Rinat <lich666black@yahoo.com>
2019-12-12 17:59:51 +0000

    use vueBemCn

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import vueBemCn from 'vue-bem-cn'
+
+import { BEM_CONFIG } from '@/config'
+
 Vue.config.productionTip = false
+
+Vue.use(vueBemCn, { delimiters: BEM_CONFIG })
 
 new Vue({
   router,
