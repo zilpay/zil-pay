6779da865f763f454b12db7a5a8047d7afc9232d
Rinat <lich666black@yahoo.com>
2019-12-12 18:02:01 +0000

    added variants config

---
+++ src/config/variants.js
@@ -0,0 +1,18 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
+export const COLOR_VARIANTS = {
+  black: 'black',
+  white: 'white',
+  gray: 'gray',
+  primary: 'primary',
+  success: 'success',
+  info: 'info',
+  danger: 'danger',
+  transparent: 'transparent'
+}
