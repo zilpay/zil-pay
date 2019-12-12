0c59c3cbf8ed1e2794dccf17672fe3b9785d6e06
Rinat <lich666black@yahoo.com>
2019-12-12 18:00:39 +0000

    added Icon componetn

---
+++ src/components/Icon.vue
@@ -0,0 +1,57 @@
+<template>
+  <img
+    :class="b({ pointer })"
+    :src="src"
+    :height="height"
+    :width="width"
+  >
+</template>
+
+<script>
+import { ICON_TYPE } from '@/config'
+
+/**
+ * @example
+ * import { ICON_TYPE } from '@/config'
+ * import Icon from '@/components/Icon'
+ * <Icon :icon="ICON_VARIANTS.logo" />
+ */
+export default {
+  name: 'Icon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: ICON_TYPE.svg
+    },
+    height: {
+      type: [Number, String],
+      default: 100
+    },
+    width: {
+      type: [Number, String],
+      default: 100
+    },
+    pointer: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    src() {
+      return `/icons/${this.icon}.${this.type}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.Icon {
+  &_pointer {
+    cursor: pointer;
+  }
+}
+</style>
