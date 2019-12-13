0d6ebd1286618ace542d6dc7e5d16c37d69e2e9f
Rinat <rinat.khasanshin@indg.com>
2019-12-13 12:15:03 +0500

    set Copyrights

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,3 +1,11 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
 <template>
   <div :class="b({ variant })">
     <slot />
@@ -9,6 +17,8 @@ import { COLOR_VARIANTS } from '@/config'
 
 /**
  * @example
+ * import { COLOR_VARIANTS } from '@/config'
+ * import Title from '@/components/Title'
  * <Title>My content</Title>
  */
 export default {
