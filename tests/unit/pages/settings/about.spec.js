1fcc6805e7757b329a3e4a856e569cc395d9ca47
Rinat <rinat.khasanshin@indg.com>
2019-12-13 19:42:47 +0500

    added test for about page

---
+++ tests/unit/pages/settings/about.spec.js
@@ -0,0 +1,26 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
+import vueBemCn from 'vue-bem-cn'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import About from 'src/pages/settings/About.vue'
+import { BEM_CONFIG } from 'src/config'
+
+const localVue = createLocalVue()
+
+localVue.use(vueBemCn, { delimiters: BEM_CONFIG })
+
+describe('pages:About.vue', () => {
+  it('Should be mount', () => {
+    const wrapper = shallowMount(About, {
+      localVue
+    })
+
+    expect(wrapper).toBeTruthy()
+  })
+})
