5033e127d229e440caea59ba90d47fc78252e886
Rinat <rinat.khasanshin@indg.com>
2019-12-13 19:24:38 +0500

    fixed router-guard and fixed routers

--- src/router/guard.js
+++ src/router/guard.js
@@ -6,15 +6,29 @@
  * -----
  * Copyright (c) 2019 ZilPay
  */
-import Create from '@/pages/Create.vue'
+import LockScreen from '@/pages/LockScreen'
 
 /**
  * Common guard for routers.
  * More info [Navigation Guards](https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards).
  * @param {*} to the target Route Object being navigated to.
  * @param {*} from the current route being navigated away from.
- * @param {*} next this function must be called to resolve the hook. The action depends on the arguments provided to.
+ * @param {Function} next this function must be called to resolve the hook.
+ * The action depends on the arguments provided to.
  */
 export default function guard(to, from, next) {
-  next(Create.name)
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    // eslint-disable-next-line no-constant-condition
+    if (true) {
+      next({
+        path: `/${LockScreen.name.toLowerCase()}`
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import guard from './guard'
 
 import Accounts from '@/pages/Accounts'
 import Connect from '@/pages/Connect'
@@ -37,7 +38,7 @@ import TxData from '@/pages/popup/TxData'
 import Import from '@/pages/accounts/Import'
 
 Vue.use(VueRouter)
-console.log(Import)
+
 // route level code-splitting
 // this generates a separate chunk (about.[hash].js) for this route
 // which is lazy-loaded when the route is visited.
@@ -48,20 +49,20 @@ const routes = [
    * No guards routers.
    */
   {
-    path: '/FirstStart',
-    name: 'FirstStart',
+    path: `/${FirstStart.name.toLowerCase()}`,
+    name: FirstStart.name,
     component: FirstStart,
     meta: { requiresAuth: false }
   },
   {
-    path: '/CreateAcc',
-    name: 'CreateAcc',
+    path: `/${CreateAcc.name.toLowerCase()}`,
+    name: CreateAcc.name,
     component: CreateAcc,
     meta: { requiresAuth: false }
   },
   {
-    path: '/LockScreen',
-    name: 'LockScreen',
+    path: `/${LockScreen.name.toLowerCase()}`,
+    name: LockScreen.name,
     component: LockScreen,
     meta: { requiresAuth: false }
   },
@@ -69,108 +70,108 @@ const routes = [
    * Guards routers.
    */
   {
-    path: '/Accounts',
-    name: 'Accounts',
+    path: `/${Accounts.name.toLowerCase()}`,
+    name: Accounts.name,
     component: Accounts,
     meta: { requiresAuth: true },
     children: [
       {
-        path: '/Import',
-        name: 'Import',
+        path: `/${Import.name.toLowerCase()}`,
+        name: Import.name,
         component: Import
       }
     ]
   },
   {
-    path: '/Connect',
-    name: 'Connect',
+    path: `/${Connect.name.toLowerCase()}`,
+    name: Connect.name,
     component: Connect,
     meta: { requiresAuth: true }
   },
   {
-    path: '/Dapps',
-    name: 'Dapps',
+    path: `/${Dapps.name.toLowerCase()}`,
+    name: Dapps.name,
     component: Dapps,
     meta: { requiresAuth: true }
   },
   {
-    path: '/Home',
-    name: 'Home',
+    path: `/${Home.name.toLowerCase()}`,
+    name: Home.name,
     component: Home,
     meta: { requiresAuth: true }
   },
   {
-    path: '/Popup',
-    name: 'Popup',
+    path: `/${Popup.name.toLowerCase()}`,
+    name: Popup.name,
     component: Popup,
     meta: { requiresAuth: true },
     children: [
       {
-        path: '/TxData',
-        name: 'TxData',
+        path: `/${TxData.name.toLowerCase()}`,
+        name: TxData.name,
         component: TxData
       }
     ]
   },
   {
-    path: '/Receive',
-    name: 'Receive',
+    path: `/${Receive.name.toLowerCase()}`,
+    name: Receive.name,
     component: Receive,
     meta: { requiresAuth: true }
   },
   {
-    path: '/Send',
-    name: 'Send',
+    path: `/${Send.name.toLowerCase()}`,
+    name: Send.name,
     component: Send,
     meta: { requiresAuth: true }
   },
   {
-    path: '/Settings',
-    name: 'Settings',
+    path: `/${Settings.name.toLowerCase()}`,
+    name: Settings.name,
     component: Settings,
     meta: { requiresAuth: true },
     children: [
       {
-        path: '/About',
-        name: 'About',
+        path: `/${About.name.toLowerCase()}`,
+        name: About.name,
         component: About
       },
       {
-        path: '/Advanced',
-        name: 'Advanced',
+        path: `/${Advanced.name.toLowerCase()}`,
+        name: Advanced.name,
         component: Advanced
       },
       {
-        path: '/General',
-        name: 'General',
+        path: `/${General.name.toLowerCase()}`,
+        name: General.name,
         component: General
       },
       {
-        path: '/Networks',
-        name: 'Networks',
+        path: `/${Networks.name.toLowerCase()}`,
+        name: Networks.name,
         component: Networks
       },
       {
-        path: '/Restore',
-        name: 'Restore',
+        path: `/${Restore.name.toLowerCase()}`,
+        name: Restore.name,
         component: Restore
       },
       {
-        path: '/Security',
-        name: 'Security',
+        path: `/${Security.name.toLowerCase()}`,
+        name: Security.name,
         component: Security
       }
     ]
   },
   {
-    path: '/TxInfo',
-    name: 'TxInfo',
+    path: `/${TxInfo.name.toLowerCase()}`,
+    name: TxInfo.name,
     component: TxInfo,
     meta: { requiresAuth: true }
   },
   {
     path: '*',
-    redirect: '/FirstStart'
+    redirect: `/${LockScreen.name.toLowerCase()}`
   }
 ]
 
@@ -179,5 +180,7 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((...args) => guard(...args))
 
 export default router
