3254e0f7b1a47c1098b4b00cb04f8988774355f2
Rinat <lich666black@yahoo.com>
2019-12-14 10:54:24 +0000

    added container component

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="b()">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container'
+}
+</script>
+
+<style lang="scss">
+/**
+ * 1. Centers the content. Yes, it's a bit opinionated.
+ * 2. See the "width vs max-width" section
+ */
+.Container {
+  width: 100%;
+  margin-right: auto; /* 1 */
+  margin-left:  auto; /* 1 */
+}
+</style>
