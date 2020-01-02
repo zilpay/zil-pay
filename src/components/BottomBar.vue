3c422024b777c923a082dd2ceaad91b3a2f389c1
Rinat <lich666black@yahoo.com>
2020-01-02 12:47:16 +0000

    added BottomBar component

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,13 +1,100 @@
 <template>
-  <div :class="b()"></div>
+  <div :class="b()">
+    <Buttom
+      v-for="el of elements"
+      :class="b('btn')"
+      :key="el.uuid"
+      :size="el.size"
+      :color="el.variant"
+      block
+      @click="$emit('click', el.event)"
+    >
+      <Icon
+        :class="b('icon')"
+        :icon="el.icon"
+        height="15"
+        width="15"
+      />
+      {{ el.value }}
+    </Buttom>
+  </div>
 </template>
 
 <script>
+import Buttom from '@/components/Button'
+import Icon from '@/components/Icon'
+
+/**
+ * Bottom action buttons.
+ * @example
+ * import { uuid } from 'uuidv4'
+ * import {
+ *   ICON_TYPE,
+ *   ICON_VARIANTS,
+ *   COLOR_VARIANTS,
+ *   SIZE_VARIANS
+ *  } from '@/config'
+ * import BottomBar from '@/components/BottomBar'
+ * const test = [
+ *  {
+ *     value: 'Send',
+ *     event: 'send',
+ *     icon: ICON_VARIANTS.add,
+ *     iconType: ICON_TYPE.svg,
+ *     variant: COLOR_VARIANTS.primary,
+ *     size: SIZE_VARIANS.sm,
+ *     uuid: uuid()
+ *  },
+ *  {
+ *     value: 'Receive',
+ *     event: 'receive',
+ *     icon: ICON_VARIANTS.add,
+ *     iconType: ICON_TYPE.svg,
+ *     variant: COLOR_VARIANTS.primary,
+ *     size: SIZE_VARIANS.sm,
+ *     uuid: uuid()
+ *   }
+ * ]
+ * <BottomBar
+ *   :elements="test"
+ *   @click="/ do somethink.../"
+ * />
+ */
 export default {
-  name: 'BottomBar'
+  name: 'BottomBar',
+  components: {
+    Buttom,
+    Icon
+  },
+  props: {
+    elements: {
+      type: Array,
+      required: true
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+.BottomBar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
 
+  display: grid;
+
+  grid-template-columns: 1fr 1fr;
+  gap: 1px;
+
+  &__btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__icon {
+    padding-right: 5px;
+  }
+}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,50 @@
 <template>
   <div :class="b()">
-    <TopBar />
+    <BottomBar :elements="test"/>
   </div>
 </template>
 
 <script>
-import TopBar from '@/components/TopBar'
+import { uuid } from 'uuidv4'
+import {
+  ICON_TYPE,
+  ICON_VARIANTS,
+  COLOR_VARIANTS,
+  SIZE_VARIANS
+} from '@/config'
+
+import BottomBar from '@/components/BottomBar'
+
+const test = [
+  {
+    value: 'Send',
+    event: 'send',
+    icon: ICON_VARIANTS.add,
+    iconType: ICON_TYPE.svg,
+    variant: COLOR_VARIANTS.primary,
+    size: SIZE_VARIANS.sm,
+    uuid: uuid()
+  },
+  {
+    value: 'Receive',
+    event: 'receive',
+    icon: ICON_VARIANTS.add,
+    iconType: ICON_TYPE.svg,
+    variant: COLOR_VARIANTS.primary,
+    size: SIZE_VARIANS.sm,
+    uuid: uuid()
+  }
+]
+
 export default {
   name: 'Home',
   components: {
-    TopBar
+    BottomBar
+  },
+  data() {
+    return {
+      test
+    }
   }
 }
 </script>
