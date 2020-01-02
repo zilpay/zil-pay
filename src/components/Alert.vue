648e9210539bdcc23a54bcd7e0af207b034e8f6f
Rinat <lich666black@yahoo.com>
2020-01-02 15:15:09 +0000

    added Alert component

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div :class="b()">
+    <div :class="b('text-alert')">
+      <slot />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +13,39 @@ export default {
 </script>
 
 <style lang="scss">
+.Alert {
+  display: grid;
 
+  grid-template-rows: 1px 80px 1px;
+
+  width: 100%;
+
+  background-color: var(--theme-color-alert);
+
+  &__text-alert {
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 5px;
+  }
+
+  &:before {
+    content: "";
+    display: block;
+
+    width: 100%;
+    height: 1px;
+
+    background-color: var(--theme-color-alert-border);
+  }
+
+  &:after {
+    content: "";
+    display: block;
+
+    width: 100%;
+    height: 1px;
+
+    background-color: var(--theme-color-alert-border);
+  }
+}
 </style>
