39013d18847f0af33dacecc7e17f8a76471fc7a0
Rinat <lich666black@yahoo.com>
2020-01-04 18:28:16 +0000

    added test for seporator component

--- src/pages/CreateAcc.vue
+++ src/pages/CreateAcc.vue
@@ -1,10 +1,34 @@
 <template>
-  <div :class="b()"></div>
+  <div :class="b()">
+    <!-- <div :class="b('wrapper')">
+      <Container :class="b('nav-bar')">
+        <Icon
+          :icon="ICON_VARIANTS.arrowLefrt"
+          width="13"
+          height="23"
+          pointer
+        />
+      </Container>
+    </div> -->
+    dsadas
+  </div>
 </template>
 
 <script>
+import { ICON_VARIANTS } from '@/config'
+
+// import Icon from '@/components/Icon'
+
 export default {
-  name: 'CreateAcc'
+  name: 'CreateAcc',
+  components: {
+    // Icon
+  },
+  data() {
+    return {
+      ICON_VARIANTS
+    }
+  }
 }
 </script>
 
---
+++ tests/unit/components/separator.spec.js
@@ -0,0 +1,32 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import vueBemCn from 'vue-bem-cn'
+import Separator from 'src/components/Separator.vue'
+import {
+  BEM_CONFIG
+} from 'src/config'
+
+const localVue = createLocalVue()
+
+localVue.use(vueBemCn, { delimiters: BEM_CONFIG })
+
+describe('components:Separator', () => {
+  const wrapper = shallowMount(Separator, {
+    localVue
+  })
+
+  it('Should can import', () => {
+    expect(Separator).toBeTruthy()
+  })
+
+  it('Should be mount', () => {
+    expect(wrapper).toBeTruthy()
+  })
+})
