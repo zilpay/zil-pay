9a931750c5930835901aacd5dc1d473a0e24b39a
Rinat <rinat.khasanshin@indg.com>
2020-01-09 16:27:51 +0500

    added Chip component

--- src/components/Chip.vue
+++ src/components/Chip.vue
@@ -1,13 +1,102 @@
 <template>
-  <div :class="b()"></div>
+  <div :class="b({ pointer, size })">
+    <div :class="b('wrapper')">
+      <div :class="b('circle')">
+        {{ circle }}
+      </div>
+      <div :class="b('content')">
+        <slot />
+      </div>
+      <CloseIcon :class="b('close', { close })" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { SIZE_VARIANS } from '@/config'
+
+import CloseIcon from '@/components/icons/Close'
+
 export default {
-  name: 'Chip'
+  name: 'Chip',
+  components: {
+    CloseIcon
+  },
+  props: {
+    circle: {
+      type: [String, Number],
+      required: false
+    },
+    pointer: {
+      type: Boolean,
+      default: false
+    },
+    close: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: String,
+      default: SIZE_VARIANS.sm
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+$default-height: 32px;
 
+.Chip {
+  display: inline-block;
+
+  height: $default-height;
+  width: 100px;
+
+  color: var(--theme-color-font);
+  line-height: $default-height;
+
+  border-radius: 16px;
+  background-color: var(--theme-color-input);
+
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__circle {
+    height: $default-height;
+    width: $default-height;
+
+    border-radius: 16px;
+    background-color: var(--accent-color-info);
+
+    text-align: center;
+  }
+
+  &__content {
+    text-indent: 5px;
+  }
+
+  &__close {
+    height: 32px;
+    width: 32px;
+    text-align: center;
+  }
+
+  &_size-xs {
+    font-size: var(--size-xs-font);
+  }
+
+  &_size-sm {
+    font-size: calc(var(--size-sm-font) - 5px);
+  }
+
+  &_size-md {
+    font-size: var(--size-md-font);
+  }
+
+  &_size-lg {
+    font-size: var(--size-lg-font);
+  }
+}
 </style>
