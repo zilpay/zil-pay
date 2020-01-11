1f521912069a6cc9f87acd98601dd7ede6892f2a
Rinat <lich666black@yahoo.com>
2020-01-11 13:15:23 +0000

    generated burger menu

---
+++ src/components/icons/Burger.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="b()">
+    <div
+      v-for="(_, index) of Array(3)"
+      :key="index"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Burger'
+}
+</script>
+
+<style lang="scss">
+
+</style>
