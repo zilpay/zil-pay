f9dc3ccf937c3103f2e0026fd5e58dd82c48231a
Rinat <lich666black@yahoo.com>
2020-01-11 14:01:23 +0000

    create some grids for home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <div :class="b()">
-    <BottomBar :elements="test"/>
+    <div :class="b('wrapper')">
+      <Icon
+        :class="b('logo')"
+        :icon="ICON_VARIANTS.zilliqaLogo"
+        width="22"
+        height="30"
+      />
+      <div :class="b('nav-bar')">
+        <Burger
+          width="20"
+          pointer
+        />
+        <div :class="b('fast-access')">
+          dsadsa
+        </div>
+        <Gear
+          width="20"
+          height="20"
+        />
+      </div>
+    </div>
+    <BottomBar :elements="bottomBar"/>
   </div>
 </template>
 
@@ -14,41 +35,72 @@ import {
 } from '@/config'
 
 import BottomBar from '@/components/BottomBar'
-
-const test = [
-  {
-    value: 'Send',
-    event: 'send',
-    icon: ICON_VARIANTS.add,
-    iconType: ICON_TYPE.svg,
-    variant: COLOR_VARIANTS.primary,
-    size: SIZE_VARIANS.sm,
-    uuid: uuid()
-  },
-  {
-    value: 'Receive',
-    event: 'receive',
-    icon: ICON_VARIANTS.add,
-    iconType: ICON_TYPE.svg,
-    variant: COLOR_VARIANTS.primary,
-    size: SIZE_VARIANS.sm,
-    uuid: uuid()
-  }
-]
+import Icon from '@/components/Icon'
+import Burger from '@/components/icons/Burger'
+import Gear from '@/components/icons/Gear'
 
 export default {
   name: 'Home',
   components: {
-    BottomBar
+    BottomBar,
+    Icon,
+    Burger,
+    Gear
   },
   data() {
     return {
-      test
+      ICON_TYPE,
+      ICON_VARIANTS,
+      COLOR_VARIANTS,
+      SIZE_VARIANS,
+      bottomBar: [
+        {
+          value: 'Receive',
+          event: 'receive',
+          icon: ICON_VARIANTS.add,
+          iconType: ICON_TYPE.svg,
+          variant: COLOR_VARIANTS.primary,
+          size: SIZE_VARIANS.sm,
+          uuid: uuid()
+        },
+        {
+          value: 'Send',
+          event: 'send',
+          icon: ICON_VARIANTS.add,
+          iconType: ICON_TYPE.svg,
+          variant: COLOR_VARIANTS.primary,
+          size: SIZE_VARIANS.sm,
+          uuid: uuid()
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="scss">
+.Home {
+  display: grid;
+  justify-content: center;
+  align-items: center;
 
+  &__wrapper,
+  &__nav-bar {
+    min-width: 360px;
+  }
+
+  &__wrapper {
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-rows: 35px 45px;
+  }
+
+  &__nav-bar {
+    display: inline-grid;
+    grid-template-columns: 45px 1fr 45px;
+    justify-items: center;
+    align-items: center;
+  }
+}
 </style>
