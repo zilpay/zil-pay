46b2c477c74a6eb4535a05edbf82dbfea079d7f6
Rinat <lich666black@yahoo.com>
2020-01-11 12:04:10 +0000

    added Wave component

---
+++ src/components/Wave.vue
@@ -0,0 +1,29 @@
+<template>
+  <img
+    :class="b()"
+    src="/illustrations/wave.svg"
+  >
+</template>
+
+<script>
+/**
+ * Just illustration wave.
+ * @example
+ * import Wave from '@/components/Wave'
+ * <Wave />
+ */
+export default {
+  name: 'Wave'
+}
+</script>
+
+<style lang="scss">
+.Wave {
+  position: fixed;
+  z-index: -1;
+  bottom: 0;
+
+  width: 100%;
+  max-width: 100vw;
+}
+</style>
