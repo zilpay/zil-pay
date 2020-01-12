5a810dccba37f2720e13ddf51ca1b265dc1407f0
Rinat <lich666black@yahoo.com>
2020-01-12 12:44:28 +0000

    added Transactions component

---
+++ src/components/Transactions.vue
@@ -0,0 +1,28 @@
+<template>
+  <Alert :class="b()">
+    <div :class="b('wrapper')">
+      Transactions
+    </div>
+  </Alert>
+</template>
+
+<script>
+import Alert from '@/components/Alert'
+
+export default {
+  name: 'Transactions',
+  components: {
+    Alert
+  }
+}
+</script>
+
+<style lang="scss">
+.Transactions {
+  width: 100vw;
+
+  &__wrapper {
+    height: 273px;
+  }
+}
+</style>
