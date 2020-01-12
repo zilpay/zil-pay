b7c150debf42d4e88e22568f3e7f057a886c022a
Rinat <lich666black@yahoo.com>
2020-01-12 12:44:04 +0000

    added FastPanel component

---
+++ src/components/FastPanel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="b()">
+    <Network
+      :width="width"
+      :height="width"
+      pointer
+    />
+    <Refresh
+      :width="width"
+      :height="width"
+      pointer
+    />
+    <Connections
+      :width="width"
+      :height="width"
+      pointer
+    />
+  </div>
+</template>
+
+<script>
+import Network from '@/components/icons/Network'
+import Refresh from '@/components/icons/Refresh'
+import Connections from '@/components/icons/Connections'
+
+export default {
+  name: 'FastPanel',
+  components: {
+    Network,
+    Refresh,
+    Connections
+  },
+  data() {
+    return {
+      width: 20,
+      height: 20
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.FastPanel {
+  display: flex;
+  justify-content: space-between;
+
+  width: 80px;
+}
+</style>
