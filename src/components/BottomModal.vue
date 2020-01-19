9cde412d5996e80bcff4df0f4c5222beb0cdbcf1
Rinat <lich666black@yahoo.com>
2020-01-19 11:45:29 +0000

    added BottomModal component

---
+++ src/components/BottomModal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div :class="b()">
+    <aside
+      v-show="value"
+      :class="b('social')"
+    >
+      <slot/>
+    </aside>
+    <div
+      v-show="value"
+      :class="b('close')"
+      @click="onModal"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BottomModal',
+  props: {
+    value: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    onModal() {
+      this.$emit('input', !this.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.BottomModal {
+  &__social,
+  &__close {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+
+  &__social {
+    z-index: 10;
+
+    background-color: var(--app-background-color);
+
+    height: fit-content;
+
+    animation: fadeInUp 0.3s;
+  }
+
+  &__close {
+    cursor: pointer;
+
+    top: 0;
+
+    background-color: var(--accent-color-black);
+
+    z-index: 5;
+
+    animation: fade;
+
+    opacity: 0.5;
+  }
+}
+</style>
