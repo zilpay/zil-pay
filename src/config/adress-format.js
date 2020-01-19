fcd7dd632153cb42db0030256386a21280e5dc8e
Rinat <lich666black@yahoo.com>
2020-01-19 14:17:51 +0000

    added config objects

---
+++ src/config/adress-format.js
@@ -0,0 +1,12 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
+export const ADDRESS_FORMAT_VARIANTS = {
+  bech32: 'Bech32',
+  base16: 'Base16'
+}
