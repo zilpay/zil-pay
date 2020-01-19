8ab730dcad5bb792f833f033f64d22fc20030671
Rinat <lich666black@yahoo.com>
2020-01-19 09:49:06 +0000

    added Item component

---
+++ src/components/Item.vue
@@ -0,0 +1,108 @@
+<template>
+  <div :class="b({ pointer })">
+    <Icon
+      v-if="src"
+      :src="src"
+      :type="ICON_TYPE.auto"
+      height="35"
+      width="35"
+      @click="onClick"
+    />
+    <div
+      :class="b('content')"
+      @click="onClick"
+    >
+      <slot/>
+    </div>
+    <Trash
+      v-if="trash"
+      height="16"
+      width="13"
+      pointer
+      @click="onRemove"
+    />
+  </div>
+</template>
+
+<script>
+import {
+  SIZE_VARIANS,
+  COLOR_VARIANTS,
+  ICON_TYPE,
+  EVENTS
+} from '@/config'
+
+import Icon from '@/components/Icon'
+
+import Trash from '@/components/icons/Trash'
+
+/**
+ * List item.
+ * @example
+ * import Item from '@/components/Item'
+ * <Item
+ *   :src="item.icon"
+ *   trash
+ *   pointer
+ *   @click="/ do something /"
+ *   @remove="/ do something /"
+ * >
+ *   any content
+ * </Item>
+ */
+export default {
+  name: 'Item',
+  components: {
+    Icon,
+    Trash
+  },
+  props: {
+    src: {
+      type: String,
+      required: false
+    },
+    trash: {
+      type: Boolean,
+      required: false
+    },
+    pointer: {
+      type: Boolean,
+      required: false
+    }
+  },
+  data() {
+    return {
+      SIZE_VARIANS,
+      COLOR_VARIANTS,
+      ICON_TYPE
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit(EVENTS.click)
+    },
+    onRemove() {
+      this.$emit(EVENTS.remove)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.Item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 330px;
+
+  &__content {
+    height: 45px;
+    width: 250px;
+  }
+
+  &_pointer {
+    cursor: pointer;
+  }
+}
+</style>
