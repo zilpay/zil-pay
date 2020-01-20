f774c274352ca6e6c8c049c37b99554d565b1c2d
Rinat <rinat.khasanshin@indg.com>
2020-01-20 19:29:33 +0500

    added test for Item component

---
+++ tests/unit/components/item.spec.js
@@ -0,0 +1,81 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import vueBemCn from 'vue-bem-cn'
+import Item from 'src/components/Item.vue'
+import {
+  BEM_CONFIG,
+  ICON_VARIANTS
+} from 'src/config'
+import { uuid } from 'uuidv4'
+
+const TEST_TEXT = uuid()
+
+const localVue = createLocalVue()
+
+localVue.use(vueBemCn, { delimiters: BEM_CONFIG })
+
+describe('components:Item', () => {
+  const wrapper = shallowMount(Item, {
+    localVue,
+    slots: {
+      default: [TEST_TEXT]
+    },
+    propsData: {
+      src: ICON_VARIANTS.statusSuccess,
+      trash: true,
+      pointer: true
+    }
+  })
+
+  it('Should be vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('Should be mount', () => {
+    expect(wrapper).toBeTruthy()
+  })
+
+  it('Should can import', () => {
+    expect(Item).toBeTruthy()
+  })
+
+  it('Should be have some class', () => {
+    expect(wrapper.classes()).toEqual([
+      Item.name,
+      `${Item.name}${BEM_CONFIG.mod}pointer`
+    ])
+  })
+
+  it('Should be some props', () => {
+    expect(wrapper.props().src).toEqual(ICON_VARIANTS.statusSuccess)
+    expect(wrapper.props().trash).toBeTruthy()
+    expect(wrapper.props().pointer).toBeTruthy()
+  })
+
+  it('Should be have some attributes', () => {
+    expect(wrapper.attributes().class).toEqual(`${Item.name} ${Item.name}${BEM_CONFIG.mod}pointer`)
+  })
+
+  it('Should be img div', () => {
+    expect(wrapper.element.tagName).toEqual('DIV')
+  })
+
+  it('Should can emit click event', () => {
+    wrapper.vm.onClick()
+
+    expect(wrapper.emitted().click).toBeTruthy()
+  })
+
+  it('Should can emit remove event', () => {
+    wrapper.vm.onRemove()
+
+    expect(wrapper.emitted().remove).toBeTruthy()
+  })
+})
