070abafd417daffe84aa017aac77eabc85836374
Rinat <lich666black@yahoo.com>
2020-01-22 18:00:46 +0000

    added ui store module

---
+++ src/store/ui.js
@@ -0,0 +1,14 @@
+export default {
+  namespaced: true,
+  state: {
+    loading: true,
+    isConnect: true,
+    conversionRate: {
+      BTC: 0,
+      USD: 0
+    }
+  },
+  mutations: {},
+  actions: {},
+  getters: {}
+}
