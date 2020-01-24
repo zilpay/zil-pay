0ea5f0b93e0b077e0f3f0b1a32674323fa8d6a1d
Rinat <rinat@Sergeys-Mac-mini.local>
2020-01-24 16:09:38 +0500

    added few loayour for Security component

--- src/pages/settings/Security.vue
+++ src/pages/settings/Security.vue
@@ -1,20 +1,67 @@
 <template>
   <div :class="b()">
     <TopBar />
+    <Container :class="b('wrapper')">
+      <RadioGroup
+        :value="getCurrent"
+        :title="autoLogoutTimerTitle"
+        :elements="getHours"
+        @input="setLockTime"
+      >
+        {{ autoLogoutTimerTitle }}
+      </RadioGroup>
+      <Separator />
+    </Container>
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters, mapMutations } from 'vuex'
+
 import TopBar from '@/components/TopBar'
+import Container from '@/components/Container'
+import Separator from '@/components/Separator'
+import RadioGroup from '@/components/RadioGroup'
 
 export default {
   name: 'Security',
   components: {
-    TopBar
+    TopBar,
+    Container,
+    Separator,
+    RadioGroup
+  },
+  data() {
+    return {
+      autoLogoutTimerTitle: 'Auto-Logout Timer (hours)'
+    }
+  },
+  computed: {
+    ...mapState('settings', [
+      'lockTime'
+    ]),
+    ...mapGetters('settings', [
+      'getHours',
+      'getCurrent'
+    ])
+  },
+  methods: {
+    ...mapMutations('settings', [
+      'setLockTime'
+    ])
   }
 }
 </script>
 
 <style lang="scss">
+.Security {
+  &__wrapper {
+    display: grid;
+    grid-gap: 30px;
+    align-items: center;
 
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+}
 </style>
