7fc88ebd0bc17c5dcd35080f56e8c939de112a7e
Rinat <lich666black@yahoo.com>
2020-01-25 11:05:28 +0000

    added Double range input

--- src/components/DoubleRange.vue
+++ src/components/DoubleRange.vue
@@ -1,125 +1,118 @@
 <template>
   <div :class="b()">
-    <div :class="b('input-range-tracker')" />
-    <div :class="b('input-range-tracker-between')" />
     <input
-      :class="b('range-lower')"
-      type="range"
-      name="range-lower"
-    >
-    <input
-      :class="b('range-upper')"
-      type="range"
-      name="range-upper"
-    >
+      v-for="(val, index) of value"
+      v-bind="$attrs"
+      :key="index"
+      :value="val"
+      :class="b(RANGE)"
+      :type="RANGE"
+      @input="onInput($event.target.value, index)"
+    />
   </div>
 </template>
 
 <script>
+import { EVENTS } from '@/config'
+
+const RANGE = 'range'
+
+/**
+ * Two range for changeing more range params.
+ * @param value Vue model schema.
+ * @event input Vue model schema.
+ * @example
+ * import DoubleRange from '@/components/DoubleRange'
+ * const range = [0, 100]
+ * <DoubleRange
+ *   v-model="range"
+ *   min="1"
+ *   max="100"
+ * />
+ */
 export default {
-  name: 'DoubleRange'
+  name: 'DoubleRange',
+  props: {
+    value: {
+      type: Array,
+      required: false
+    }
+  },
+  data() {
+    return {
+      RANGE
+    }
+  },
+  methods: {
+    onInput(value, index) {
+      const currentState = this.value
+
+      if (isNaN(value)) {
+        return null
+      }
+
+      currentState[index] = Number(value)
+
+      this.$emit(EVENTS.input, currentState)
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-$input-range-thumb-height: 20px;
-$input-range-tracker-height: 10px;
-$input-range-lower-thumb-color: LightSkyBlue;
-$input-range-upper-thumb-color: RebeccaPurple;
-
 .DoubleRange {
+  display: flex;
+	flex-direction: column;
   position: relative;
-	width: 200px;
-	height: $input-range-thumb-height;
 
-  &__input-range-tracker {
+  &__range {
     position: absolute;
+    top: 0;
     left: 0;
-    top: 50%;
+
     width: 100%;
-    height: $input-range-tracker-height;
-    margin-top: -$input-range-tracker-height / 2;
-    border-radius: 10px;
-    background: LightGray;
+    -webkit-appearance: none;
+    background: var(--accent-color-info);
+    height: 2px;
+    outline: none;
+    outline-offset: 0;
     pointer-events: none;
+
+    &::-moz-focus-inner {
+      border-style: none;
+    }
+
+    &::-moz-focus-outer {
+      border-style: none;
+    }
+
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      background: var(--accent-color-info);
+      width: 10px;
+      height: 10px;
+      border-radius: 100%;
+      border: none;
+      cursor: pointer;
+      pointer-events: all;
+      z-index: 11;
+    }
+
+    &::-moz-range-thumb {
+      background: var(--accent-color-info);
+      width: 10px;
+      height: 10px;
+      border-radius: 10px;
+      border: none;
+      cursor: pointer;
+      appearance: none;
+      pointer-events: all;
+    }
+
+    &::-moz-range-track {
+      outline: none;
+      pointer-events: none;
+    }
   }
-
-  &__input-range-tracker-between {
-    position: absolute;
-    left: 0;
-    top: 50%;
-    width: 0;
-    height: $input-range-tracker-height;
-    margin-top: -$input-range-tracker-height / 2;
-    border-radius: 10px;
-    background: LightSalmon;
-    pointer-events: none;
-  }
-}
-input[type="range"] {
-	appearance: none;
-	position: absolute;
-	display: block;
-	width: 100%;
-	background: transparent;
-
-	&.DoubleRange__display-upper {
-		z-index: 1;
-	}
-}
-
-input[type="range"]:not(*:root) {
-	pointer-events: none;
-}
-
-input[type="range"]::-webkit-slider-runnable-track {
-	background: transparent;
-}
-
-input[type="range"] {
-	&::-webkit-slider-thumb,
-	&::-webkit-media-slider-thumb {
-		appearance: none;
-		width: 20px;
-		height: 20px;
-		border: 0;
-		border-radius: 50%;
-		pointer-events: auto;
-	}
-}
-input[name="range-lower"] {
-	&::-webkit-slider-thumb,
-	&::-webkit-media-slider-thumb {
-		background: $input-range-lower-thumb-color;
-	}
-}
-
-input[name="range-upper"] {
-	&::-webkit-slider-thumb,
-	&::-webkit-media-slider-thumb {
-		background: $input-range-upper-thumb-color;
-	}
-}
-
-// Firefox
-input[type="range"]::-moz-range-track {
-	background: transparent;
-}
-
-input[type="range"]::-moz-range-thumb {
-	appearance: none;
-	width: 20px;
-	height: 20px;
-	border: 0;
-	border-radius: 50%;
-	pointer-events: auto;
-}
-
-input[name="range-lower"]::-moz-range-thumb {
-	background: $input-range-lower-thumb-color;
-}
-
-input[name="range-upper"]::-moz-range-thumb {
-	background: $input-range-upper-thumb-color;
 }
 </style>
