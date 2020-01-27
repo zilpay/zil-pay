70c8282cc39e2c82142d06c7f95bbc3f959077f9
Rinat <rinat@Sergeys-Mac-mini.local>
2020-01-27 15:39:21 +0500

    added test for TransactionCard component

---
+++ tests/unit/components/transaction-card.spec.js
@@ -0,0 +1,58 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import vueBemCn from 'vue-bem-cn'
+import TransactionCard, { TX_TYPES } from 'src/components/TransactionCard.vue'
+import { BEM_CONFIG, ICON_VARIANTS } from 'src/config'
+
+const localVue = createLocalVue()
+const TEST_TRANASCTION = {
+  Info: 'Contract Txn, Shards Match of the sender and reciever',
+  TranID: '8bd99abe0f280d5df9fd515eadd93870902ecf5a5c661cfc37bca22fc657f5fd',
+  amount: '10000000000000',
+  nonce: 55,
+  toAddr: '0xE8A997e359AC2A1e891dBDf7fc7558623bB0eaD2',
+  status: 'confirmed'
+}
+
+localVue.use(vueBemCn, { delimiters: BEM_CONFIG })
+
+describe('components:TransactionCard', () => {
+  const wrapper = shallowMount(TransactionCard, {
+    localVue,
+    propsData: {
+      transaction: TEST_TRANASCTION
+    }
+  })
+
+  it('Should be import', () => {
+    expect(TransactionCard).toBeTruthy()
+  })
+
+  it('Should be vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('Should be mount', () => {
+    expect(wrapper).toBeTruthy()
+  })
+
+  it('checks the props', () => {
+    expect(wrapper.props().transaction).toEqual(TEST_TRANASCTION)
+  })
+
+  it('Should can know tx type', () => {
+    expect(wrapper.vm.txType).toBe(TX_TYPES.trigger)
+  })
+
+  it('Should can know status', () => {
+    expect(wrapper.vm.statusIcon).toBe(ICON_VARIANTS.statusSuccess)
+  })
+
+})
