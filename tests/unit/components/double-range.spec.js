d18e11efcb593a17628d221ab99616b8a8362f77
Rinat <rinat@Sergeys-Mac-mini.local>
2020-01-27 15:38:05 +0500

    added test for DoubleRange component

---
+++ tests/unit/components/double-range.spec.js
@@ -0,0 +1,61 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import vueBemCn from 'vue-bem-cn'
+import DoubleRange from 'src/components/DoubleRange.vue'
+import { BEM_CONFIG } from 'src/config'
+
+const localVue = createLocalVue()
+
+localVue.use(vueBemCn, { delimiters: BEM_CONFIG })
+
+describe('components:DoubleRange', () => {
+  const wrapper = shallowMount(DoubleRange, {
+    localVue,
+    propsData: {
+      value: [0, 100]
+    }
+  })
+
+  it('Should can import', () => {
+    expect(DoubleRange).toBeTruthy()
+  })
+
+  it('Should be mount', () => {
+    expect(wrapper).toBeTruthy()
+  })
+
+  it('Should be vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  it('checks the props', () => {
+    expect(wrapper.props().value).toEqual([0, 100])
+  })
+
+  it('Should be have some class', () => {
+    expect(wrapper.classes()).toEqual([
+      DoubleRange.name
+    ])
+  })
+
+  it('Should be div tag', () => {
+    expect(wrapper.element.tagName).toEqual('DIV')
+  })
+
+  it('Should can emit input event', () => {
+    wrapper.vm.onInput(10, 0)
+
+    expect(wrapper.emitted().input).toEqual([
+      [
+        [10, 100]
+      ]
+    ])
+  })
+})
