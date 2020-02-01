d81cddf414887afee2a2b88f00a47331e62c0196
Rinat <lich666black@yahoo.com>
2020-02-01 14:11:45 +0000

    added new features for Title component

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -3,7 +3,14 @@
     :class="b({ variant, font, size })"
     @click="onClick"
   >
-    <slot />
+    <input
+      v-if="changeable"
+      :value="value"
+      :class="b('input', { variant, font, size })"
+      type="text"
+      @change="onInput"
+    >
+    <slot v-if="!changeable"/>
   </div>
 </template>
 
@@ -11,6 +18,13 @@
 import { FONT_VARIANTS, SIZE_VARIANS, EVENTS } from '@/config'
 
 /**
+ * Text title component.
+ * @param variant Color variant from config.
+ * @param font Font variant from config.
+ * @param size Size variant from config.
+ * @param changeable Can use as v-model.
+ * @event click Mouse click event.
+ * @event input When changer info.
  * @example
  * import { COLOR_VARIANTS } from '@/config'
  * import Title from '@/components/Title'
@@ -30,11 +44,22 @@ export default {
     size: {
       type: String,
       default: SIZE_VARIANS.lg
+    },
+    changeable: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: String,
+      required: false
     }
   },
   methods: {
     onClick() {
       this.$emit(EVENTS.click)
+    },
+    onInput(event) {
+      this.$emit(EVENTS.input, event.target.value)
     }
   }
 }
@@ -44,47 +69,73 @@ export default {
 .Title {
   color: var(--theme-color-font);
 
-  &_font-medium {
+  &__input {
+    max-width: fit-content;
+    max-width: 250px;
+    max-height: 20px;
+
+    color: var(--theme-color-font);
+    text-indent: -1px;
+
+    background-color: transparent;
+    border: 0;
+    outline: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  &_font-medium,
+  &__input_font-medium {
     font-family: var(--font-family-medium);
   }
 
-  &_font-bold {
+  &_font-bold,
+  &__input_font-bold {
     font-family: var(--font-family-bold);
   }
 
-  &_font-light {
+  &_font-light,
+  &__input_font-light {
     font-family: var(--font-family-light);
   }
 
-  &_size-xs {
+  &_size-xs,
+  &__input_size-xs {
     font-size: var(--size-xs-font);
   }
 
-  &_size-sm {
+  &_size-sm,
+  &__input_size-sm {
     font-size: var(--size-sm-font);
   }
 
-  &_size-md {
+  &_size-md,
+  &__input_size-md {
     font-size: var(--size-md-font);
   }
 
-  &_size-lg {
+  &_size-lg,
+  &__input_size-lg {
     font-size: var(--size-lg-font);
   }
 
-  &_variant-success {
+  &_variant-success,
+  &__input_variant-success {
     color: var(--accent-color-success);
   }
 
-  &_variant-info {
+  &_variant-info,
+  &__input_variant-info {
     color: var(--accent-color-info);
   }
 
-  &_variant-danger {
+  &_variant-danger,
+  &__input_variant-danger {
     color: var(--accent-color-danger);
   }
 
-  &_variant-warning {
+  &_variant-warning,
+  &__input_variant-warning {
     color: var(--accent-color-warning);
   }
 }
