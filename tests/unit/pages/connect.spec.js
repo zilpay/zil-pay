75372b880bb5be6aa523e43bcd525f2cbf9d6224
Rinat <figurative63@protonmail.com>
2020-02-09 11:47:09 +0000

    fixed connect page test

--- tests/unit/pages/connect.spec.js
+++ tests/unit/pages/connect.spec.js
@@ -6,19 +6,23 @@
  * -----
  * Copyright (c) 2019 ZilPay
  */
+import Vuex from 'vuex'
 import vueBemCn from 'vue-bem-cn'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Connect from 'src/pages/Connect.vue'
 import { BEM_CONFIG } from 'src/config'
+import store from 'src/store'
 
 const localVue = createLocalVue()
 
 localVue.use(vueBemCn, { delimiters: BEM_CONFIG })
+localVue.use(Vuex)
 
 describe('pages:Connect.vue', () => {
   it('Should be mount', () => {
     const wrapper = shallowMount(Connect, {
-      localVue
+      localVue,
+      store
     })
 
     expect(wrapper).toBeTruthy()
