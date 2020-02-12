1e82c77ee1149201a3c3e074b0f96f12aa6e3f91
Rinat <rinat@Sergeys-Mac-mini.local>
2020-02-12 18:14:19 +0500

    added UiPanel panel component

---
+++ src/components/UiPanel.vue
@@ -0,0 +1,68 @@
+<template>
+  <div :class="b()">
+    <div :class="b('theme')">
+      <Icon
+        v-show="selectedTheme === themes[0]"
+        :icon="ICON_VARIANTS.moon"
+        height="40"
+        width="40"
+        pointer
+        @click="setTheme(themes[1])"
+      />
+      <Icon
+        v-show="selectedTheme === themes[1]"
+        :icon="ICON_VARIANTS.sun"
+        height="40"
+        width="40"
+        pointer
+        @click="setTheme(themes[0])"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+import uiStore from '@/store/ui'
+
+import { ICON_VARIANTS } from '@/config'
+
+import Icon from '@/components/Icon'
+
+/**
+ * Show icons for change theme.
+ * @example
+ * import UiPanel from '@/components/UiPanel'
+ * <UiPanel />
+ */
+export default {
+  name: 'UiPanel',
+  components: {
+    Icon
+  },
+  data() {
+    return {
+      ICON_VARIANTS
+    }
+  },
+  computed: {
+    ...mapState(uiStore.STORE_NAME, [
+      uiStore.STATE_NAMES.selectedTheme,
+      uiStore.STATE_NAMES.themes
+    ])
+  },
+  methods: {
+    ...mapMutations(uiStore.STORE_NAME, [
+      uiStore.MUTATIONS_NAMES.setTheme
+    ])
+  }
+}
+</script>
+
+<style lang="scss">
+.UiPanel {
+  position: absolute;
+  top: 15px;
+  right: 30px;
+}
+</style>
