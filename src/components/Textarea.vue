29300928e27ffdd29af2138eb6c32c4469065b9e
Rinat <rinat@Sergeys-Mac-mini.local>
2020-02-19 12:32:58 +0500

    added Textarea component

---
+++ src/components/Textarea.vue
@@ -0,0 +1,61 @@
+<template>
+  <textarea
+    v-bind="$attrs"
+    :class="b({ round })"
+    :value="value"
+    @input="onInput"
+  >
+    <slot />
+  </textarea>
+</template>
+
+<script>
+import { EVENTS } from '@/config'
+
+export default {
+  name: 'Textarea',
+  props: {
+    value: {
+      type: [String, Number],
+      required: false
+    },
+    round: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    onInput(event) {
+      const { value } = event.target
+
+      this.$emit(EVENTS.input, value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.Textarea {
+  display: inline-block;
+
+  width: 100%;
+  height: 100px;
+
+  padding-left: 15px;
+  padding-top: 15px;
+
+  border: 0;
+
+  font-size: var(--size-sm-font);
+  color: var(--theme-color-font);
+  background-color: var(--theme-color-input);
+
+  outline: none;
+  resize: none;
+  overflow: hidden;
+
+  &_round {
+    border-radius: var(--default-border-radius);
+  }
+}
+</style>
