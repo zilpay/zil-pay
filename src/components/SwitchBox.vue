2d34182399d22b20b39ccd7069f0f9c05aa1b0b9
Rinat <rinat@Sergeys-Mac-mini.local>
2020-02-19 19:31:12 +0500

    added SwitchBox component

---
+++ src/components/SwitchBox.vue
@@ -0,0 +1,82 @@
+<template>
+  <label :class="b()">
+    <input
+      v-bind="$attrs"
+      :value="value"
+      :class="b('input')"
+      type="checkbox"
+      @input="onInput"
+    >
+    <span :class="b('slider')" />
+  </label>
+</template>
+
+<script>
+import { uuid } from 'uuidv4'
+
+import { EVENTS } from '@/config'
+
+export default {
+  name: 'SwitchBox',
+  props: {
+    value: {
+      type: Boolean,
+      required: false
+    }
+  },
+  data() {
+    return {
+      uuid: uuid()
+    }
+  },
+  methods: {
+    onInput() {
+      this.$emit(EVENTS.input, !this.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.SwitchBox {
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 25px;
+
+  &__input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+
+    &:checked + .SwitchBox__slider:before {
+      transform: translateX(23px);
+      background-color: var(--accent-color-success);
+    }
+  }
+
+  &__slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: var(--theme-color-separator);
+    border-radius: 34px;
+    transition: .4s;
+
+    &:before {
+      position: absolute;
+      content: "";
+      height: 20px;
+      width: 20px;
+      left: 3px;
+      bottom: 3px;
+      border-radius: 50%;
+      background-color: var(--accent-color-white);
+      transition: .4s;
+    }
+  }
+}
+</style>
