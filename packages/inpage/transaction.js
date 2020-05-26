e17fcb055672a575f58e803979091bdf62f268a3
Rinat <lich666black@gmail.com>
2020-05-26 16:39:11 +0000

    added tranasctions

---
+++ packages/inpage/transaction.js
@@ -0,0 +1,52 @@
+import { validation } from '@zilliqa-js/util/dist/index'
+import HTTPProvider from './provider'
+import Wallet from './wallet'
+import ERRORS from './errors'
+
+export class Transaction {
+  get _toAddr() {
+    return this.toAddr.replace('0x', '').toLowerCase()
+  }
+
+  constructor(params, priority = false) {
+    this.version = params.version
+    this.toAddr = params.toAddr
+    this.nonce = params.nonce
+    this.amount = String(params.amount)
+    this.code = params.code || ''
+    this.data = params.data || ''
+    this.signature = params.signature
+    this.gasPrice = String(params.gasPrice)
+    this.gasLimit = String(params.gasLimit)
+    this.priority = params.priority
+
+    if (priority) {
+      this.priority = true
+    }
+
+    if (!this.toAddr || !validation.isAddress(this.toAddr)) {
+      throw ERRORS.IncorectAddress
+    }
+  }
+}
+
+export class TransactionFactory {
+
+  constructor(provider, wallet) {
+    if (!(provider instanceof HTTPProvider)) {
+      throw ERRORS.HTTPProviderInstance
+    } else if (!(wallet instanceof Wallet)) {
+      throw ERRORS.WalletInstance
+    }
+    this.provider = provider
+    this.wallet = wallet
+  }
+
+  new(...args) {
+    return new Transaction(...args)
+  }
+
+  payment() {
+    throw ERRORS.DisabledMethod
+  }
+}
