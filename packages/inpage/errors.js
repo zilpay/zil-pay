03ba08c0a9432a97d0c5868ed0fd78104097f392
Rinat <lich666black@gmail.com>
2020-05-26 16:39:00 +0000

    added errors intances

--- packages/inpage/errors.js
+++ packages/inpage/errors.js
@@ -1,5 +1,22 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2019 ZilPay
+ */
 export default {
+  InitParams: new Error('Cannot deploy without code or initialisation parameters.'),
+  ShouldArray: new Error('should be array'),
+  VariableNameRequired: new Error('Variable name required'),
+  IncorectAddress: new Error('Incorrect address format'),
+  HTTPProviderInstance: new Error('should be HTTPProvider instance.'),
+  WalletInstance: new Error('should be Wallet instance.'),
+  TransactionFactoryInstance: new Error('should be TransactionFactory instance.'),
   Connect: new Error('User is\'t connections.'),
+  DisabledMethod: new Error('This method not allowed in ZIlPay'),
   Disabled: new Error('ZilPay is disabled.'),
+  ContractHasntDeployed: new Error('Contract has not been deployed!'),
   Args: new Error('Set arguments as zilPay.wallet.observableTransaction(hash, hash, hash)')
 }
