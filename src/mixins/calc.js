4beee07506aeec0bb8372cd5b2787c28e1dc6c4c
Rinat <lich666black@gmail.com>
2020-07-02 09:17:38 +0000

    changed calc formula

--- src/mixins/calc.js
+++ src/mixins/calc.js
@@ -6,9 +6,24 @@
  * -----
  * Copyright (c) 2019 ZilPay
  */
-import { BN, units } from '@zilliqa-js/util'
-
+import Big from 'big.js'
+import { DEFAULT_TOKEN } from 'config'
 import { fromZil } from '@/filters'
+
+
+const _li = Big(10 ** 6)
+
+function gasFee(gasPrice, gasLimit) {
+  const _gasPrice = Big(gasPrice).round()
+  const _gasLimit = Big(gasLimit).round()
+  const _fee = _gasLimit.mul(_gasPrice)
+  const fee = _fee.div(_li)
+
+  return {
+    _fee,
+    fee
+  }
+}
 
 export default {
   methods: {
@@ -18,13 +33,18 @@ export default {
      * @param {String} fee Tx gas fee.
      * @param {String} balance Account balance.
      */
-    calcIsInsufficientFunds(amount, gasLimit, gasPrice, balance) {
+    calcIsInsufficientFunds(amount, gasLimit, gasPrice, balance, symbol, decimals) {
       try {
-        const _gasLimit = new BN(gasLimit)
-        const _gasPrice = new BN(gasPrice)
-        const _fee = units.toQa(_gasLimit.mul(_gasPrice), units.Units.Li)
-        const _amount = new BN(amount)
-        const _balance = new BN(balance)
+        let _fee = Big(0)
+
+        if (symbol === DEFAULT_TOKEN.symbol) {
+          _fee = gasFee(gasPrice, gasLimit)._fee
+        }
+
+        const _fact = Big(10 ** decimals)
+        const _Famount = Big(amount)
+        const _balance = Big(balance).round()
+        const _amount = _Famount.mul(_fact).round()
         const _txAmount = _fee.add(_amount)
 
         return _balance.lt(_txAmount)
@@ -37,18 +57,25 @@ export default {
      * @param {String} fee Tx gas fee.
      * @param {String} balance Account balance.
      */
-    calcMaxAmount(gasLimit, gasPrice, balance, decimals) {
+    calcMaxAmount(gasLimit, gasPrice, balance, decimals, symbol) {
       if (Number(balance) === 0) {
-        return Number(balance)
+        return String(balance)
       }
 
-      const _gasLimit = new BN(gasLimit)
-      const _gasPrice = new BN(gasPrice)
-      const _fee = units.toQa(_gasLimit.mul(_gasPrice), units.Units.Li)
-      const _balance = new BN(balance)
-      const _amount = _balance.sub(_fee)
+      try {
+        let _fee = Big(0)
 
-      return fromZil(_amount, decimals, false)
+        if (symbol === DEFAULT_TOKEN.symbol) {
+          _fee = gasFee(gasPrice, gasLimit)._fee
+        }
+
+        const _balance = Big(balance).round()
+        const _amount = _balance.sub(_fee)
+
+        return fromZil(_amount, decimals, false)
+      } catch (err) {
+        return '0'
+      }
     },
     /**
      * Calculate gas fee amount.
@@ -56,10 +83,7 @@ export default {
      * @param {Number} gasPrice Li.
      */
     calcFee(gasLimit, gasPrice) {
-      const factor = Math.pow(10, -6) // 10 ^ -6
-      const amount = gasLimit * gasPrice * factor
-
-      return amount.toFixed(3)
+      return String(gasFee(gasPrice, gasLimit).fee)
     }
   }
 }
