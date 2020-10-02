529ec487b3671d54b6c62cd7678e2531d2df6e7c
Rinat <lich666black@gmail.com>
2020-10-02 14:09:06 +0000

    added some tests for modal store

--- tests/unit/store/modal.spec.js
+++ tests/unit/store/modal.spec.js
@@ -79,4 +79,73 @@ describe('store:modal', () => {
 
     expect(keys.length).toBe(0)
   })
+
+  it('try toggle SideBar', () => {
+    expect(state.sideBarSettings).toBe(false)
+
+    mutations.toggleSideBarSettings(state)
+
+    expect(state.sideBarSettings).toBe(true)
+
+    mutations.toggleSideBarSettings(state)
+
+    expect(state.sideBarSettings).toBe(false)
+  })
+
+  it('try toggle sendModal', () => {
+    expect(state.sideBarSettings).toBe(false)
+    expect(state.receiveModal.show).toBe(false)
+    expect(state.accountModal.show).toBe(false)
+    expect(state.sendModal.show).toBe(false)
+    expect(state.sendModal.step).toBe(0)
+
+    mutations.setShowSendModal(state)
+    mutations.setNextStep(state)
+
+    expect(state.sideBarSettings).toBe(false)
+    expect(state.receiveModal.show).toBe(false)
+    expect(state.accountModal.show).toBe(false)
+    expect(state.sendModal.show).toBe(true)
+    expect(state.sendModal.step).toBe(1)
+
+    mutations.setShowSendModal(state)
+  })
+
+  it('try toggle receiveModal', () => {
+    expect(state.sideBarSettings).toBe(false)
+    expect(state.receiveModal.show).toBe(false)
+    expect(state.accountModal.show).toBe(false)
+    expect(state.sendModal.show).toBe(false)
+    expect(state.receiveModal.step).toBe(0)
+
+    mutations.setShowReceiveModal(state)
+    mutations.setNextStep(state)
+
+    expect(state.sideBarSettings).toBe(false)
+    expect(state.receiveModal.show).toBe(true)
+    expect(state.accountModal.show).toBe(false)
+    expect(state.sendModal.show).toBe(false)
+    expect(state.receiveModal.step).toBe(1)
+
+    mutations.setShowReceiveModal(state)
+  })
+
+  it('try toggle set Show Account Modal', () => {
+    expect(state.sideBarSettings).toBe(false)
+    expect(state.receiveModal.show).toBe(false)
+    expect(state.accountModal.show).toBe(false)
+    expect(state.sendModal.show).toBe(false)
+    expect(state.accountModal.step).toBe(0)
+
+    mutations.setShowAccountModal(state)
+    mutations.setNextStep(state)
+
+    expect(state.sideBarSettings).toBe(false)
+    expect(state.receiveModal.show).toBe(false)
+    expect(state.accountModal.show).toBe(true)
+    expect(state.sendModal.show).toBe(false)
+    expect(state.accountModal.step).toBe(1)
+
+    mutations.setShowSendModal(state)
+  })
 })
