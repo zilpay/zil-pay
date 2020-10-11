706f5c2f700a8cac044848a084a89b9e09cf70f8
Rinat <lich666black@gmail.com>
2020-10-11 18:19:50 +0500

    changed to finance

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -4,7 +4,7 @@
     <AddMenu @click="contactModal = true"/>
     <div :class="b('wrapper')">
       <Title :size="SIZE_VARIANS.md">
-        {{ $options.name }}
+        {{ local.CONTACTS }}
       </Title>
       <ul :class="b('scroll')">
         <li
