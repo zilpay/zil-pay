2fcc93331dc43508ff236efabdc51eee014369d6
evesnow91 <31395583+evesnow91@users.noreply.github.com>
2020-10-19 12:35:42 +0800

    Update Contacts.vue

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -4,7 +4,7 @@
     <AddMenu @click="contactModal = true"/>
     <div :class="b('wrapper')">
       <Title :size="SIZE_VARIANS.md">
-        {{ local.FINANCES }}
+        {{ local.CONTACTS }}
       </Title>
       <ul :class="b('scroll')">
         <li
@@ -17,7 +17,7 @@
           </div>
           <P
             :font="FONT_VARIANTS.regular"
-            @click="onSelectContact(contact)"
+            @click="onSelectContact(contact)"z
           >
             {{ contact.name }}
           </P>
