1eac91a53ecf0a098a775f53ceedc388cd4a70eb
Rinat <lich666black@gmail.com>
2021-09-08 17:05:19 +0300

    added config

---
+++ config/contracts.ts
@@ -0,0 +1,14 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2021 ZilPay
+ */
+
+export enum Contracts {
+  ZIL_SWAP = '0xBa11eB7bCc0a02e947ACF03Cc651Bfaf19C9EC00',
+  SSN = '0xB780e8095b8BA85A7145965ed632b3B774ac51cE',
+  SCAM = '0xe66593414Ba537e965b5e0eB723a58b4d1fACc89'
+}
---
+++ config/fields.ts
@@ -0,0 +1,26 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2021 ZilPay
+ */
+
+export enum Fields {
+  VAULT = 'vault',
+  VAULT_IMPORTED = 'importedvault',
+  CONFIG = 'config',
+  WALLET = 'wallet',
+  SELECTED_NET = 'selectednet',
+  BLOCK_NUMBER = 'blocknumber',
+  TRANSACTIONS = 'transactions',
+  CONFIRM_TX = 'confirm',
+  STATIC = 'static',
+  CONNECT_DAPP = 'connect',
+  THEME = 'theme',
+  CONTACTS = 'contacts',
+  SELECTED_COIN = 'selectedcoin',
+  TOKENS = 'tokens',
+  SSN = 'ssnlist'
+}
