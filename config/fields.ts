492ea0c1d08cad54bb8f2f3061854d2559a4df7d
Rinat <lich666black@gmail.com>
2021-09-10 16:00:14 +0300

    added ipfs controll

--- config/fields.ts
+++ config/fields.ts
@@ -23,5 +23,6 @@ export enum Fields {
   CONTACTS = 'contacts',
   SELECTED_COIN = 'selectedcoin',
   TOKENS = 'tokens',
-  SSN = 'ssnlist'
+  SSN = 'ssnlist',
+  SELECTED_IPFS = 'selected_ipfs'
 }
---
+++ config/ipfs-list.ts
@@ -0,0 +1,36 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(hiccaru)
+ * -----
+ * Modified By: the developer formerly known as Rinat(hiccaru) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2021 ZilPay
+ */
+
+export const IPFS_LIST = Object.freeze([
+  {
+    name: 'General',
+    url: 'https://ipfs.io/ipfs',
+    value: '0'
+  },
+  {
+    name: 'Pinata',
+    url: 'https://gateway.pinata.cloud/ipfs',
+    value: '0'
+  },
+  {
+    name: 'Gateway',
+    url: 'https://gateway.ipfs.io/ipfs',
+    value: '0'
+  },
+  {
+    name: 'Infura',
+    url: 'https://ipfs.infura.io/ipfs',
+    value: '0'
+  },
+  {
+    name: 'Cloudflare',
+    url: 'https://cloudflare-ipfs.com/ipfs',
+    value: '0'
+  }
+]);
