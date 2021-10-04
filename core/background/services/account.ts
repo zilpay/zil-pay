e424549084aa333dc8a6fffd7aa0edfa3a771626
Rinat <lich666black@gmail.com>
2021-10-04 16:34:09 +0300

    generated ZRC controllers

---
+++ core/background/services/account.ts
@@ -0,0 +1,17 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(hiccaru)
+ * -----
+ * Modified By: the developer formerly known as Rinat(hiccaru) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2021 ZilPay
+ */
+import type { Account } from 'types/account';
+
+type Wallet = {
+  selectedAddress: number;
+  identities: Account[];
+}
+
+export class AccountController {
+}
