69f14534a4c8b3d62e595ce6e76bce11640ba4e1
Rinat <lich666black@gmail.com>
2021-10-15 15:50:52 +0300

    added apps to core

--- core/background/wallet/apps.ts
+++ core/background/wallet/apps.ts
@@ -6,21 +6,83 @@
  * -----
  * Copyright (c) 2021 ZilPay
  */
+import type { StreamResponse } from 'types/stream';
 import type { ZIlPayCore } from './core';
+import type { AppConnect } from 'types/app-connect';
 
 export class ZilPayApps {
   private readonly _core: ZIlPayCore;
-
-  public async signMessage() {}
-  public async signTransaction() {}
 
   constructor(core: ZIlPayCore) {
     this._core = core;
   }
 
-  public async addApp() {}
+  public async addApp(app: AppConnect, sendResponse: StreamResponse) {
+    try {
+      await this._core.apps.add(app);
 
-  public async removeApp() {}
+      sendResponse({
+        resolve: this._core.apps.connections
+      });
+    } catch (err) {
+      sendResponse({
+        reject: err.message
+      });
+    }
+  }
 
-  public async clearApps() {}
+  public async addConfirm(app: AppConnect, sendResponse: StreamResponse) {
+    try {
+      await this._core.apps.addConfirm(app);
+
+      /// TODO: open popup.
+      sendResponse({
+        resolve: app
+      });
+    } catch (err) {
+      sendResponse({
+        reject: err.message
+      });
+    }
+  }
+
+  public requestConnections(sendResponse: StreamResponse) {
+    try {
+      sendResponse({
+        resolve: this._core.apps.connections
+      });
+    } catch (err) {
+      sendResponse({
+        reject: err.message
+      });
+    }
+  }
+
+  public async removeApp(index: number, sendResponse: StreamResponse) {
+    try {
+      await this._core.apps.rm(index);
+
+      sendResponse({
+        resolve: this._core.apps.connections
+      });
+    } catch (err) {
+      sendResponse({
+        reject: err.message
+      });
+    }
+  }
+
+  public async clearApps(sendResponse: StreamResponse) {
+    try {
+      await this._core.apps.reset();
+
+      sendResponse({
+        resolve: this._core.apps.connections
+      });
+    } catch (err) {
+      sendResponse({
+        reject: err.message
+      });
+    }
+  }
 }
