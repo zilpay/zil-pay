8ff1a62216285f9097613abd74df5e127090c9df
Rinat <lich666black@gmail.com>
2021-10-20 17:36:55 +0300

    added guard store to popup

--- popup/main.ts
+++ popup/main.ts
@@ -1,5 +1,12 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(hiccaru)
+ * -----
+ * Modified By: the developer formerly known as Rinat(hiccaru) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2021 ZilPay
+ */
 import App from './App.svelte';
-import { Wallet } from './backend';
 
 const app = new App({
 	target: document.body
