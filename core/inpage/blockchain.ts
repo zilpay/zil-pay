6ebdb7391a11a6d9486e0cd223789126ee8f816c
Rinat <lich666black@gmail.com>
2021-10-21 15:25:53 +0300

    added some methods to blockchain

--- core/inpage/blockchain.ts
+++ core/inpage/blockchain.ts
@@ -46,7 +46,7 @@ export class Blockchain {
    * If it does not contained an error, but `receipt.success` is `false`, then
    * a rejected Transaction instance is returned.
    */
-   async getTransaction(hash: string): Promise<Transaction> {
+  public async getTransaction(hash: string): Promise<Transaction> {
     assert(Boolean(hash), 'hash is REQUIRED');
     const fixedhash = CryptoUtils.toHex(hash);
 
@@ -58,5 +58,92 @@ export class Blockchain {
     assert(Boolean(result), String(error['message']));
 
     return new Transaction(result as TransactionParams);
+  }
+
+  /**
+   * getRecentTransactions
+   *
+   * Gets a list of recent transactions
+   */
+  public getRecentTransactions() {
+    return this.#provider.send(RPCMethod.GetRecentTransactions);
+  }
+
+   /**
+   * getTransactionsForTxBlock
+   *
+   * Gets all transactions for a given TxBlock, grouped by shard id
+   */
+  public getTransactionsForTxBlock(txBlock: number) {
+    assert(!isNaN(txBlock), 'Txblock should be number');
+    return this.#provider.send(
+      RPCMethod.GetTransactionsForTxBlock,
+      String(txBlock)
+    );
+  }
+
+  /**
+   * getBlockChainInfo
+   */
+  public getBlockChainInfo() {
+    return this.#provider.send(RPCMethod.GetBlockchainInfo);
+  }
+
+  /**
+   * getShardingStructure
+   */
+  public getShardingStructure() {
+    return this.#provider.send(RPCMethod.GetShardingStructure);
+  }
+
+  /**
+   * getDSBlock
+   *
+   * Get details of a Directory Service block by block number.
+   */
+  public getDSBlock(blockNum: number) {
+    assert(!isNaN(blockNum), 'blockNum should be number');
+
+    return this.#provider.send(RPCMethod.GetDSBlock, String(blockNum));
+  }
+
+  /**
+   * getLatestDSBlock
+   *
+   * Get details of the most recent Directory Service block.
+   */
+  public getLatestDSBlock() {
+    return this.#provider.send(RPCMethod.GetLatestDSBlock);
+  }
+
+  /**
+   * getNumDSBlocks
+   *
+   * Gets the number of DS blocks that the network has processed.
+   */
+  public getNumDSBlocks() {
+    return this.#provider.send(RPCMethod.GetNumDSBlocks);
+  }
+
+  /**
+   * getDSBlockRate
+   *
+   * Gets the average rate of DS blocks processed per second
+   */
+  public getDSBlockRate() {
+    return this.#provider.send(RPCMethod.GetDSBlockRate);
+  }
+
+  /**
+   * getDSBlockListing
+   *
+   * Get a paginated list of Directory Service blocks. Pass in page number as
+   * parameter. Returns a maxPages variable that specifies the max number of
+   * pages. 1 - latest blocks, maxPages - oldest blocks.
+   */
+  public getDSBlockListing(max: number) {
+    assert(!isNaN(max), 'max should be number');
+
+    return this.#provider.send(RPCMethod.DSBlockListing, String(max));
   }
 }
