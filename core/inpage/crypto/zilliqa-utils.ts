4c2aa791b4aa4a324a04a092e1aaeb1ac3128231
Rinat <lich666black@gmail.com>
2021-10-24 11:16:32 +0300

    added qa methods

--- core/inpage/crypto/zilliqa-utils.ts
+++ core/inpage/crypto/zilliqa-utils.ts
@@ -10,10 +10,36 @@ import BN from 'bn.js';
 import Long from 'long';
 import { Validator } from './validator';
 
+export const Units = {
+  Li: BigInt('1'),
+  Qa: BigInt('1000000'),
+  Zil: BigInt('1000000000000')
+};
+
+export function fromQa(
+  qa: bigint | string | number,
+  unit: bigint
+) {
+  const value = BigInt(qa);
+  const res = value / unit;
+
+  return String(res);
+}
+
+export function toQa(
+  qa: bigint | number | string,
+  unit: bigint
+) {
+  const value = BigInt(qa);
+  const res = value * unit;
+  return String(res);
+}
+
 export const ZilliqaUtils = {
   BN,
   Long,
   validation: Validator,
-  bytes: '',
-  units: {}
+  units: {
+    Units
+  }
 };
