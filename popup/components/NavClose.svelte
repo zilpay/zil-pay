330276f9680437ab0d1d7f2b9900c983451f7d24
Rinat <lich666black@gmail.com>
2021-10-29 16:01:55 +0300

    added NavClose

---
+++ popup/components/NavClose.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { pop } from 'svelte-spa-router';
+	import CloseIcon from './Close.svelte';
+
+  export let title: string;
+</script>
+
+<nav>
+  <h1>
+    {title}
+  </h1>
+  <a on:click={pop}>
+    <CloseIcon />
+  </a>
+</nav>
+
+<style type="text/scss">
+  @import "../styles/mixins";
+  h1 {
+    @include fluid-font(320px, 720px, 30px, 55px);
+  }
+  nav {
+    @include flex-between-row;
+    max-width: 900px;
+    width: calc(100vw - 30px);
+    align-items: center;
+  }
+</style>
--- popup/pages/Restore.svelte
+++ popup/pages/Restore.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
+	import { pop } from 'svelte-spa-router';
   import { fly } from 'svelte/transition';
   import flyTransition from 'popup/transitions/fly';
+	import { _ } from 'popup/i18n';
 
+	import NavClose from '../components/NavClose.svelte';
+
+  const handleBack = () => {
+    pop();
+  };
 </script>
 
 <main
-in:fly={flyTransition.in}
-out:fly={flyTransition.out}
+  in:fly={flyTransition.in}
+  out:fly={flyTransition.out}
 >
-<h1>Restore!</h1>
+  <NavClose title={$_('restore.title')}/>
 </main>
 
 <style type="text/scss">
+  @import "../styles/mixins";
+  main {
+		background-color: var(--background-color);
+		height: 100vh;
+
+		@include flex-center-top-column;
+  }
 </style>
