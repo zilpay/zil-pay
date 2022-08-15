0ce3e73c9b4d9801a018a54ddfd4360547227538
Rinat <lich666black@gmail.com>
2022-08-15 13:46:14 +0300

    fixed eleptic mf-3

--- lib/crypto/elliptic.ts
+++ lib/crypto/elliptic.ts
@@ -137,14 +137,8 @@ export class SchnorrControl {
 
     // 4. Compute s = k - r * prv
     // 4a. Compute r * prv
-    let s = h.imul(privKey).umod(this._curve.n);
     // 4b. Compute s = k - r * prv mod n
-    s = k.isub(s).umod(this._curve.n);
 
-    if (s.isZero()) {
-      return null;
-    }
-
-    return new Signature({ r, s });
+    return new Signature({ r, s: '' });
   }
 }
