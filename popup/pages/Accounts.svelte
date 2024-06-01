175ac150e7174c63a5b9dbc2d3e93b5fada654ed
Hicaru <hicc0rus@gmail.com>
2024-06-01 11:22:45 +0500

    added ru

--- popup/pages/Accounts.svelte
+++ popup/pages/Accounts.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-	import { push } from 'svelte-spa-router';
-	import { _ } from 'popup/i18n';
-	import walletStore from 'popup/store/wallet';
-  import { fade } from 'svelte/transition';
+	import { push } from "svelte-spa-router";
+	import { _ } from "popup/i18n";
+	import walletStore from "popup/store/wallet";
+	import { fade } from "svelte/transition";
 
-	import { selectAccount } from 'popup/backend/wallet';
+	import { selectAccount } from "popup/backend/wallet";
 
-	import NavClose from '../components/NavClose.svelte';
-	import AccountsModal from '../modals/Accounts.svelte';
+	import NavClose from "../components/NavClose.svelte";
+	import AccountsModal from "../modals/Accounts.svelte";
 
 	const onSelectAccount = async ({ detail }) => {
 		await selectAccount(detail);
-		push('/');
+		push("/");
 	};
 </script>
 
 <main in:fade>
-	<NavClose title={$_('accounts.title')}/>
+	<NavClose title={$_("accounts.title")} />
 	<AccountsModal
 		list={$walletStore.identities}
 		index={$walletStore.selectedAddress}
