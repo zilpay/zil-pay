58c42a8ed928ebc9114ffb328bf1bd5f184984c2
Rinat <rinat@Rinats-MacBook-Air.local>
2025-05-10 12:24:53 +0900

    fixed config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node', 
-    globals: true,        
-    setupFiles: '__tests__/setupTests.ts', 
-    include: ['__tests__/**/*.test.{ts,tsx}'], 
+    environment: 'node',
+    globals: true,
+    setupFiles: '__tests__/setupTests.ts',
+    include: ['__tests__/**/*.test.{ts,tsx}'],
     coverage: {
-      provider: 'v8',     
+      provider: 'v8',
       reporter: ['text', 'html'],
+      reportsDirectory: 'stats/coverage',
     },
+    reporters: [
+      'default',
+      ['html', { outputDir: 'stats/report' }]
+    ],
   },
 });
