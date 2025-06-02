94cf6d4c67c39442af2ffb284e2b9bd92bd18725
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-02 18:22:56 +0900

    added ntru prime key gen tests

--- crypto/ntrup.ts
+++ crypto/ntrup.ts
@@ -1,5 +1,12 @@
 import { ChaCha20Rng } from "@hicaru/chacharand.js";
-import { generateKeyPair, params761, PrivKey, PubKey } from "@hicaru/ntrup.js";
+import {
+  generateKeyPair,
+  params761,
+  PrivKey,
+  PubKey,
+  staticBytesEncrypt,
+} from "@hicaru/ntrup.js";
+import { randomBytes } from "../crypto/random";
 
 export function ntruKeysFromSeed(seedBytes: Uint8Array): {
   pk: PubKey;
@@ -9,4 +16,10 @@ export function ntruKeysFromSeed(seedBytes: Uint8Array): {
   const pqRng = ChaCha20Rng(seedPq);
 
   return generateKeyPair(pqRng, params761);
+}
+
+export function ntruEncrypt(pk: PubKey, plaintext: Uint8Array) {
+  let seed = randomBytes(32);
+  const rng = ChaCha20Rng(seed);
+  // staticBytesEncrypt();
 }
