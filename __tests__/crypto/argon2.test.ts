f8b2c59868a74aa03679d05fb51dd61b38ad34b6
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-03 16:42:57 +0900

    added argon2 and test for it

--- __tests__/crypto/argon2.test.ts
+++ __tests__/crypto/argon2.test.ts
@@ -1,9 +1,30 @@
 import { describe, expect, it } from "vitest";
 import { deriveArgon2Key } from '../../crypto/argon2';
-import { Config } from "@hicaru/argon2-pure.js";
+import { Config, Variant, Version } from "@hicaru/argon2-pure.js";
+import { APP_ID } from "../../config/argon2";
 
 describe("argon2", () => {
-  it("should shuffle an array of numbers", () => {
-    deriveArgon2Key(new Uint8Array([]), "", Config.owasp1());
+  it('should derive the correct key', () => {
+    const password = new TextEncoder().encode('test_password');
+    const salt = 'some_salt';
+    const config = new Config(
+      APP_ID,          
+      64,              
+      2,               
+      65536,           
+      new Uint8Array(),
+      1,               
+      Variant.Argon2id,
+      Version.Version13
+    );
+    const key = deriveArgon2Key(password, salt, config);
+    const expected = new Uint8Array([
+      241, 5, 105, 168, 214, 152, 5, 38, 199, 60, 34, 215, 245, 198, 217, 49, 108, 140,
+      86, 183, 92, 152, 168, 92, 64, 48, 241, 204, 238, 247, 198, 88, 18, 126, 75, 177,
+      211, 74, 244, 234, 39, 92, 32, 255, 148, 131, 9, 175, 213, 251, 48, 220, 238, 146,
+      16, 147, 132, 15, 46, 51, 176, 134, 238, 69
+    ]);
+    
+    expect(key).toEqual(expected);
   });
 });
--- crypto/argon2.ts
+++ crypto/argon2.ts
@@ -1,3 +1,16 @@
-import { Config } from '@hicaru/argon2-pure.js';
+import { Config, hashRaw } from '@hicaru/argon2-pure.js';
+import { WALLET_SALT } from '../config/argon2';
 
-export function deriveArgon2Key(password: Uint8Array, salt: string, config: Config) {}
+export function deriveArgon2Key(password: Uint8Array, salt: string, config: Config) {
+  const textEncoder = new TextEncoder();
+  const saltBytes = textEncoder.encode(salt);
+
+    const combinedSalt = new Uint8Array(saltBytes.length + WALLET_SALT.length);
+
+    combinedSalt.set(saltBytes, 0);
+    combinedSalt.set(WALLET_SALT, saltBytes.length);
+
+    const hash = hashRaw(password, combinedSalt, config);
+
+    return hash;
+}
