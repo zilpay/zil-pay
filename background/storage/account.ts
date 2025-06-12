5b0875bd11eeda46616271ebc4441e6013a459dc
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-12 17:08:48 +0900

    added account derive from seed

--- background/storage/account.ts
+++ background/storage/account.ts
@@ -1,6 +1,10 @@
-import { derivePrivateKey } from '../../crypto/bip32';
+import { utils } from 'aes-js';
+import { deriveFromPrivateKeyPublicKey, derivePrivateKey } from '../../crypto/bip32';
 import { DerivationPath } from '../../crypto/bip49';
 import { AddressType } from './address-type';
+import { ChainConfig } from './chain';
+import { fromZilPubKey, toBech32Address } from '../../lib/zilliqa';
+import { ETHEREUM, ZILLIQA } from '../../config/slip44';
 
 export interface Bip32Account {
   name: string;
@@ -28,10 +32,42 @@ export class Account {
     this.index = data.index as number;
   }
 
-  async fromBip39(bip32Account: Bip32Account, slip44: number, seed: Uint8Array) {
-    const hdPath = new DerivationPath(slip44, bip32Account.index);
+  static async fromBip39(bip32Account: Bip32Account, chain: ChainConfig, seed: Uint8Array) {
+    const hdPath = new DerivationPath(chain.slip44, bip32Account.index);
     const privateKey = await derivePrivateKey(seed, hdPath.getPath());
+    const pubKey = await deriveFromPrivateKeyPublicKey(privateKey, chain.slip44);
+    const addrType = (() => {
+      switch (chain.slip44) {
+        case ZILLIQA:
+          return AddressType.Bech32;
+        case ETHEREUM:
+          return AddressType.EthCheckSum;
+        default:
+          return AddressType.EthCheckSum;
+      }
+    })();
+    const addr = await (async () => {
+      switch (chain.slip44) {
+        case ZILLIQA:
+          const base16 = await fromZilPubKey(pubKey);
+          return await toBech32Address(utils.hex.fromBytes(base16));
+        case ETHEREUM:
+          throw new Error();
+        default:
+          throw new Error();
+      }
+    })();
+    const account = new Account({
+      addr,
+      addrType,
+      name: bip32Account.name,
+      index: bip32Account.index,
+      pubKey: utils.hex.fromBytes(pubKey),
+      chainHash: chain.hash(),
+      slip44: chain.slip44,
+      chainId: chain.chainId,
+    });
 
-    const pubKey = '';
+    return account;
   }
 }
---
+++ lib/zilliqa/index.ts
@@ -0,0 +1,4 @@
+export * from './bech32';
+export * from './checksum';
+export * from './config';
+export * from './pubkey';
