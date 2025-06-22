4808c548cffb281c31e6a73b6ade81ea0e2bf702
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-22 17:30:12 +0900

    added ConfirmState

--- background/storage/confirm.ts
+++ background/storage/confirm.ts
@@ -1,1 +1,43 @@
-export class ConfirmState {}
+import type { MinScillaParams, TokenTransferMetadata, TransactionRequestEVM } from "types/tx";
+
+export class ConfirmState {
+  uuid: string;
+  title: string;
+  icon: string;
+  token?: TokenTransferMetadata;
+  scilla?: MinScillaParams;
+  signMessageScilla?: string;
+  evm?: TransactionRequestEVM;
+  signPersonalMessageEVM?: string;
+  signTypedDataJsonEVM?: string;
+
+  constructor(data: Record<string, unknown>) {
+    this.uuid = data.uuid as string;
+    this.title = data.title as string;
+    this.icon = data.icon as string;
+
+    if (data.token) {
+      this.token = data.token as TokenTransferMetadata;
+    }
+
+    if (data.scilla) {
+      this.scilla = data.scilla as MinScillaParams;
+    }
+
+    if (data.evm) {
+      this.evm = data.evm as TransactionRequestEVM;
+    }
+
+    if (data.signPersonalMessageEVM) {
+      this.signPersonalMessageEVM = data.signPersonalMessageEVM as string;
+    }
+
+    if (data.signPersonalMessageEVM) {
+      this.signPersonalMessageEVM = data.signPersonalMessageEVM as string;
+    }
+
+    if (data.signTypedDataJsonEVM) {
+      this.signTypedDataJsonEVM = data.signTypedDataJsonEVM as string;
+    }
+  }
+}
---
+++ types/tx.d.ts
@@ -0,0 +1,40 @@
+export interface MinScillaParams {
+  chainId?: number;
+  nonce?: string | number;
+  gasPrice?: string | number;
+  gasLimit?: string | number;
+  priority?: boolean;
+  code?: string;
+  data?: string;
+  toAddr: string;
+  amount: string;
+}
+
+export interface TokenTransferMetadata {
+  decimals: number;
+  symbol: string;
+  name: string;
+  to: string;
+}
+
+
+export interface TransactionRequestEVM {
+  nonce?: number;
+  from?: string;
+  to?: string;
+  value?: string;
+  gasLimit?: number;
+  data?: Uint8Array;
+  maxFeePerGas?: string;
+  maxPriorityFeePerGas?: string;
+  gasPrice?: string;
+  chainId?: number;
+  accessList?: AccessListItem[];
+  blobVersionedHashes?: string[];
+  maxFeePerBlobGas?: string;
+}
+
+export interface AccessListItem {
+  address: string;
+  storageKeys: string[];
+}
