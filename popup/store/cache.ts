97887e748ca16a809784453fcdf8242fbc451e1f
Rinat <you@example.com>
2025-08-04 18:31:43 +0500

    added chain to chache

--- popup/store/cache.ts
+++ popup/store/cache.ts
@@ -1,9 +1,11 @@
-import { writable } from 'svelte/store';
+import type { IChainConfigState } from 'background/storage';
 import type { IKeyPair } from 'types/wallet';
+import { writable } from 'svelte/store';
 
 export interface ICache {
   verifyPhrase?: string[];
   keyPair?: IKeyPair;
+  chain?: IChainConfigState;
 }
 
 export const cacheStore = writable<ICache>({});
