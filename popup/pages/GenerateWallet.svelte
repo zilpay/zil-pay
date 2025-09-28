3f6403de88a24f58e5fb5d32ef43d6785cebf0dd
Rinat <you@example.com>
2025-09-28 11:28:49 +0500

    added GenerateWallet.svelte

--- popup/pages/GenerateWallet.svelte
+++ popup/pages/GenerateWallet.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import { _ } from '../i18n';
-  import { pop, push } from '../router/navigation';
+  import { push } from '../router/navigation';
   import NavBar from '../components/NavBar.svelte';
   import WalletOption from '../components/WalletOption.svelte';
-  import Bip39Icon from '../components/icons/BIP39.svelte';
-  import GridIcon from '../components/icons/Bill.svelte';
-  import LockIcon from '../components/icons/Locker.svelte';
+  import BillIcon from '../components/icons/Bill.svelte';
+  import BincodeIcon from '../components/icons/Bincode.svelte';
 
   type GenerateOption = {
     id: string;
@@ -21,7 +20,7 @@
       id: 'bip39',
       titleKey: 'generateWallet.bip39.title',
       descriptionKey: 'generateWallet.bip39.description',
-      icon: Bip39Icon,
+      icon: BillIcon,
       disabled: false,
       action: () => handleBip39()
     },
@@ -29,26 +28,14 @@
       id: 'private-key',
       titleKey: 'generateWallet.privateKey.title',
       descriptionKey: 'generateWallet.privateKey.description',
-      icon: LockIcon,
+      icon: BincodeIcon,
       disabled: false,
       action: () => handlePrivateKey()
-    },
-    {
-      id: 'slip0039',
-      titleKey: 'generateWallet.slip0039.title',
-      descriptionKey: 'generateWallet.slip0039.description',
-      icon: GridIcon,
-      disabled: true,
-      action: () => handleSlip0039()
-    },
+    }
   ];
 
   function handleBip39() {
     push('/generate-bip39');
-  }
-
-  function handleSlip0039() {
-    push('/generate-wallet/slip0039');
   }
 
   function handlePrivateKey() {
@@ -57,20 +44,19 @@
 </script>
 
 <div class="page-container generate-wallet">
-  <NavBar title={$_('generateWallet.title')} onBack={() => pop()} />
+  <NavBar title={$_('generateWallet.title')} />
   
   <div class="options-container">
-    <div class="generate-options">
-      {#each generateOptions as option}
-        <WalletOption
-          title={$_(option.titleKey)}
-          description={$_(option.descriptionKey)}
-          icon={option.icon}
-          disabled={option.disabled}
-          onclick={() => option.action()}
-        />
-      {/each}
-    </div>
+    {#each generateOptions as option}
+      <WalletOption
+        title={$_(option.titleKey)}
+        description={$_(option.descriptionKey)}
+        icon={option.icon}
+        disabled={option.disabled}
+        onclick={option.action}
+        showArrow={true}
+      />
+    {/each}
   </div>
 </div>
 
@@ -79,22 +65,16 @@
     display: flex;
     flex-direction: column;
     height: 100vh;
-    background-color: var(--background-color);
-    padding: 0 20px 20px;
+    background-color: var(--color-neutral-background-base);
+    padding: 0 16px;
     box-sizing: border-box;
   }
 
   .options-container {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    overflow-y: auto;
-    padding: 8px 0;
-  }
-
-  .generate-options {
+    padding-top: 24px;
     display: flex;
     flex-direction: column;
     gap: 16px;
+    overflow-y: auto;
   }
 </style>
